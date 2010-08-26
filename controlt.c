9fdd9284df7765299763abc8f75124382f4fc75b
Fabio M. Di Nitto <fdinitto@redhat.com>
2010-08-26 10:38:06 +0200

    control thread: add check to see if the thread does init properly
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- controlt.c
+++ controlt.c
@@ -8,9 +8,12 @@
 
 static pthread_t ctrl_thread;
 static pthread_mutex_t ctrl_mutex;
+int control_thread_active = 0;
 
 static void *control_thread(void *arg)
 {
+	control_thread_active = 1;
+
 	for (;;) {
 		sleep(1);
 		logt_print(LOG_DEBUG, "I AM A THREAD!\n");
@@ -29,6 +32,12 @@ int start_control_thread(void)
 	rv = pthread_create(&ctrl_thread, NULL, control_thread, NULL);
 	if (rv < 0)
 		logt_print(LOG_INFO, "Unable to create control thread: %s\n", strerror(errno));
+
+	while (control_thread_active == 0)
+		sleep(1);
+
+	if (control_thread_active < 0)
+		rv = control_thread_active;
 
 	return rv;
 }
