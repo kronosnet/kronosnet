c8ed20e0c5ccb41c2898af71396955ed70698b95
Fabio M. Di Nitto <fdinitto@redhat.com>
2010-10-07 10:57:48 +0200

    Clean master to start refactoring
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- controlt.h
+++
@@ -1,7 +0,0 @@
-#ifndef __CONTROLT_H__
-#define __CONTROLT_H__
-
-int start_control_thread(void);
-int stop_control_thread(void);
-
-#endif
--- utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UTILS_H__
-#define __UTILS_H__
-
-extern int do_read(int fd, void *buf, size_t count);
-extern int do_write(int fd, void *buf, size_t count);
-extern int str_explode(char *src, char **dest, int *pos);
-
-#endif
