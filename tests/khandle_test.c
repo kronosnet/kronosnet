66ee94390c49440a35eb6819d8d405bf38a996f7
Federico Simoncelli <federico.simoncelli@gmail.com>
2010-10-15 14:09:49 +0200

    knet: add loop count

--- tests/khandle_test.c
+++ tests/khandle_test.c
@@ -8,22 +8,22 @@
 #include "ring.h"
 #include "utils.h"
 
-int my_action(struct knet_host *host, void *data);
-
-int my_action(struct knet_host *host, void *data)
-{
-	host->active = 1;
-	return 0;
-}
-
 #define HOST_LIST_SIZE 8192
 #define HOST_LIST_LOOP 64
 
 struct knet_host *host_list[HOST_LIST_SIZE];
 
+static int my_action(struct knet_host *host, void *data)
+{
+	host->active = 1;
+	*(size_t *) data += 1;
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	int i, j;
+	size_t loopnum;
 	knet_handle_t knet_h;
 
 	knet_h = knet_handle_new();
@@ -33,15 +33,20 @@ int main(int argc, char *argv[])
 		knet_host_add(knet_h, host_list[i]);
 	}
 
+	loopnum = 0;
+
 	for (i = 0; i < HOST_LIST_SIZE; i++) {
 		for (j = 0; j < HOST_LIST_LOOP; j++)
-			knet_host_foreach(knet_h, my_action, NULL);
+			knet_host_foreach(knet_h, my_action, &loopnum);
 		knet_host_remove(knet_h, host_list[i]);
 	}
 
 	for (i = 0; i < HOST_LIST_SIZE; i++) {
 		free(host_list[i]);
+		host_list[i] = NULL;
 	}
+
+	printf("loop count: %lu times\n", loopnum);
 
 	return 0;
 }
