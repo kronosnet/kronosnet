e51552907516670d4c65d89bb1a5fdc78f0b196c
Federico Simoncelli <federico.simoncelli@gmail.com>
2010-10-27 11:09:03 +0200

    netutils: add tests for strtoaddr and addrtostr

--- tests/netutils_test.c
+++ tests/netutils_test.c
@@ -2,9 +2,120 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 #include "netutils.h"
 #include "utils.h"
+
+static void check_ipv4(void)
+{
+	int err;
+	char *buf[2];
+	struct sockaddr_in addr, addrck;
+
+	memset(&addr, 0, sizeof(struct sockaddr_in));
+	memset(&addrck, 0, sizeof(struct sockaddr_in));
+
+	log_info("Checking strtoaddr on 192.168.0.1:50000");
+
+	addrck.sin_family = AF_INET;
+	addrck.sin_addr.s_addr = htonl(0xc0a80001); /* 192.168.0.1 */
+	addrck.sin_port = htons(50000);
+
+	err = strtoaddr("192.168.0.1", "50000",
+			(struct sockaddr *) &addr, sizeof(struct sockaddr_in));
+
+	if (err != 0) {
+		log_error("Unable to convert 192.168.0.1:50000");
+		exit(EXIT_FAILURE);
+	}
+
+	if (memcmp(&addr, &addrck, sizeof(struct sockaddr_in)) != 0) {
+		errno = EINVAL;
+		log_error("Check on 192.168.0.1:50000 failed");
+		exit(EXIT_FAILURE);
+	}
+
+	log_info("Checking addrtostr on 192.168.0.1:50000");
+
+	err = addrtostr((struct sockaddr *) &addrck,
+					sizeof(struct sockaddr_in), buf);
+
+	if (err != 0) {
+		log_error("Unable to convert 192.168.0.1:50000");
+		exit(EXIT_FAILURE);
+	}
+
+	if (strcmp(buf[0], "192.168.0.1") != 0) {
+		errno = EINVAL;
+		log_error("Wrong address conversion: %s", buf[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	if (strcmp(buf[1], "50000") != 0) {
+		errno = EINVAL;
+		log_error("Wrong port conversion: %s", buf[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	addrtostr_free(buf);
+}
+
+static void check_ipv6(void)
+{
+	int err;
+	char *buf[2];
+	struct sockaddr_in6 addr, addrck;
+
+	memset(&addr, 0, sizeof(struct sockaddr_in6));
+	memset(&addrck, 0, sizeof(struct sockaddr_in6));
+
+	log_info("Checking strtoaddr on [fd00::1]:50000");
+
+	addrck.sin6_family = AF_INET6;
+	addrck.sin6_addr.s6_addr16[0] = htons(0xfd00); /* fd00::1 */
+	addrck.sin6_addr.s6_addr16[7] = htons(0x0001);
+	addrck.sin6_port = htons(50000);
+
+	err = strtoaddr("fd00::1", "50000",
+			(struct sockaddr *) &addr, sizeof(struct sockaddr_in6));
+
+	if (err != 0) {
+		log_error("Unable to convert [fd00::1]:50000");
+		exit(EXIT_FAILURE);
+	}
+
+	if (memcmp(&addr, &addrck, sizeof(struct sockaddr_in6)) != 0) {
+		errno = EINVAL;
+		log_error("Check on 192.168.0.1:50000 failed");
+		exit(EXIT_FAILURE);
+	}
+
+	log_info("Checking addrtostr on [fd00::1]:50000");
+
+	err = addrtostr((struct sockaddr *) &addrck,
+					sizeof(struct sockaddr_in6), buf);
+
+	if (err != 0) {
+		log_error("Unable to convert 192.168.0.1:50000");
+		exit(EXIT_FAILURE);
+	}
+
+	if (strcmp(buf[0], "fd00::1") != 0) {
+		errno = EINVAL;
+		log_error("Wrong address conversion: %s", buf[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	if (strcmp(buf[1], "50000") != 0) {
+		errno = EINVAL;
+		log_error("Wrong port conversion: %s", buf[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	addrtostr_free(buf);
+}
 
 int main(int argc, char *argv[])
 {
@@ -12,7 +123,11 @@ int main(int argc, char *argv[])
 	char *buf[2];
 	struct sockaddr_storage address;
 
-	if (argc != 3) {
+	if (argc == 1) { /* automated tests */
+		check_ipv4();
+		check_ipv6();
+		return 0;
+	} else if (argc != 3) {
 		printf("usage: %s [host] [port]\n", argv[0]);
 		exit(EXIT_SUCCESS);
 	}
