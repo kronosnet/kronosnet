0f984836bbaed892a419fc124e2e4c094e3eee77
Fabio M. Di Nitto <fdinitto@redhat.com>
2010-11-12 12:36:41 +0100

    vty: add support to configure ip addresses on knet ifaces
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- cfg.h
+++ cfg.h
@@ -6,11 +6,19 @@
 #include "knet.h"
 #include "ring.h"
 
+struct knet_cfg_ip {
+	char *ipaddr;
+	char *prefix;
+	int  active;
+	struct knet_cfg_ip *next;
+};
+
 struct knet_cfg_eth {
 	char name[IFNAMSIZ];
 	uint8_t node_id;
 	int mtu;
 	int default_mtu;
+	struct knet_cfg_ip *knet_ip;
 };
 
 struct knet_cfg {
@@ -22,10 +30,15 @@ struct knet_cfg {
 
 struct knet_cfg_top {
 	char *conffile;
-	char *ip_addr;
-	char *port;
+	char *vty_ip;
+	char *vty_port;
 	struct knet_cfg *knet_cfg;
 };
+
+struct knet_cfg_ip *knet_get_ip(struct knet_cfg *knet_iface,
+				const char *ipaddr, const char *prefix,
+				const int create);
+void knet_destroy_ip(struct knet_cfg *knet_iface, struct knet_cfg_ip *knet_ip);
 
 struct knet_cfg *knet_get_iface(const char *name, const int create);
 void knet_destroy_iface(struct knet_cfg *knet_iface);
