06b399db6cd86d734221c8030a6ea1b887393f3e
Fabio M. Di Nitto <fdinitto@redhat.com>
2012-11-17 08:08:32 +0100

    libknet: basic prototyping of HOST_INFO messages
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- libknet/libknet.h
+++ libknet/libknet.h
@@ -67,6 +67,42 @@ struct knet_host {
 	struct knet_host *next;
 };
 
+#define KNET_HOST_INFO_LINK_UP_DOWN 0
+#define KNET_HOST_INFO_LINK_TABLE   1
+
+struct knet_hinfo_link {
+	uint8_t			khl_link_id;
+	uint8_t			khl_link_dynamic;
+	uint8_t			khl_link_priority;
+	uint64_t		khl_link_latency;
+	char			khl_link_dst_ipaddr[KNET_MAX_HOST_LEN];
+	char			khl_link_dst_port[6];
+} __attribute__((packed));
+
+struct knet_hinfo_link_table {
+	uint16_t		khlt_node_id;
+	uint8_t			khlt_local; /* we have this node connected locally */
+	struct knet_hinfo_link	khlt_link[KNET_MAX_LINK]; /* info we send about each link in the node */
+} __attribute__((packed));
+
+union knet_hinfo_dtype {
+	struct {
+		uint8_t		khdt_link_id;
+		uint8_t		khdt_link_status;
+	} link_up_down;
+	struct {
+		uint16_t	khdt_host_entries;
+		uint8_t		khdt_host_maps[0]; /* array of knet_hinfo_link_table[khdt_host_entries] */
+	} link_table __attribute__((packed));
+} __attribute__((packed));
+
+struct knet_hinfo_data {			/* this is sent in kf_data */
+	uint8_t			khd_type;	/* link_up_down / link_table */
+	uint8_t			khd_bcast;	/* bcast/ucast */
+	uint16_t		khd_dst_node_id;/* used only if in ucast mode */
+	union knet_hinfo_dtype  khd_dype;
+} __attribute__((packed));
+
 union knet_frame_data {
 	struct {
 		seq_num_t	kfd_seq_num;
