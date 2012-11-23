1ab701ce164979311d8f1ab25e6134fa91964882
Fabio M. Di Nitto <fdinitto@redhat.com>
2012-11-23 16:16:39 +0100

    libknet: fix locking within logging
    we cannot grab a lock during logging because something else
    might be already using a lock.
    
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- libknet/common.c
+++ libknet/common.c
@@ -162,9 +162,6 @@ void log_msg(knet_handle_t knet_h, uint8_t subsystem, uint8_t msglevel,
 	msg.subsystem = subsystem;
 	msg.msglevel = msglevel;
 
-	if (pthread_rwlock_rdlock(&knet_h->list_rwlock) != 0)
-		return;
-
 	va_start(ap, fmt);
 	vsnprintf(msg.msg, sizeof(msg.msg) - 1, fmt, ap);
 	va_end(ap);
@@ -172,13 +169,10 @@ void log_msg(knet_handle_t knet_h, uint8_t subsystem, uint8_t msglevel,
 	while (byte_cnt < sizeof(struct knet_log_msg)) {
 		len = write(knet_h->logfd, &msg, sizeof(struct knet_log_msg) - byte_cnt);
 		if (len <= 0)
-			goto out_unlock;
+			return;
 
 		byte_cnt += len;
 	}
-
-out_unlock:
-	pthread_rwlock_unlock(&knet_h->list_rwlock);
 
 	return;
 }
