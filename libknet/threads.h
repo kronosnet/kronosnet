5e6d3758bd5b7fbc8d74b7b7655bcfc17bb21e12
Fabio M. Di Nitto <fdinitto@redhat.com>
2012-12-19 14:28:38 +0100

    and maybe add the threads.* files to git might help.. just a tiny little bit
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

---
+++ libknet/threads.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2010-2012 Red Hat, Inc.  All rights reserved.
+ *
+ * Authors: Fabio M. Di Nitto <fabbione@kronosnet.org>
+ *          Federico Simoncelli <fsimon@kronosnet.org>
+ *
+ * This software licensed under GPL-2.0+, LGPL-2.0+
+ */
+
+#ifndef __THREADS_H__
+#define __THREADS_H__
+
+#define KNET_MAX_EVENTS 8
+
+void *_handle_tap_to_links_thread(void *data);
+void *_handle_recv_from_links_thread(void *data);
+void *_handle_heartbt_thread(void *data);
+void *_handle_dst_link_handler_thread(void *data);
+
+#endif
