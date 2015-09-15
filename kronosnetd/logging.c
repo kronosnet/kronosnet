63d61843d5abed8d5f3ee35d30634f3f5e0fc57b
Fabio M. Di Nitto <fdinitto@redhat.com>
2015-09-15 11:35:49 +0200

    [knetd] fix libqb logging filter
    Thanks to Chrissie for the awesome work as usual
    
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- kronosnetd/logging.c
+++ kronosnetd/logging.c
@@ -35,14 +35,14 @@ void logging_init_defaults(int debug, int daemonize, const char *logfile)
 		qb_log_format_set(QB_LOG_STDERR, "%t %N [%p]: %b");
 		qb_log_ctl(QB_LOG_STDERR, QB_LOG_CONF_ENABLED, QB_TRUE);
 		qb_log_filter_ctl(QB_LOG_STDERR, QB_LOG_FILTER_ADD,
-				  QB_LOG_FILTER_FUNCTION, "", level);
+				  QB_LOG_FILTER_FUNCTION, "*", level);
 	}
 
 	filetarget = qb_log_file_open(logfile);
 	qb_log_ctl(filetarget, QB_LOG_CONF_ENABLED, QB_TRUE);
 	qb_log_format_set(filetarget, "%t %N [%p]: %b");
 	qb_log_filter_ctl(filetarget, QB_LOG_FILTER_ADD,
-			  QB_LOG_FILTER_FUNCTION, "", level);
+			  QB_LOG_FILTER_FUNCTION, "*", level);
 
 	qb_log_thread_start();
 	qb_log_ctl(QB_LOG_SYSLOG, QB_LOG_CONF_THREADED, QB_TRUE);
