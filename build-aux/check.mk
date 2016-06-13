7e7300d6bb798963528e3f95f0abaa23a3ccbbb4
Fabio M. Di Nitto <fdinitto@redhat.com>
2016-06-13 15:09:00 +0200

    [tests] fix running tests with valgrin under libtool
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- build-aux/check.mk
+++ build-aux/check.mk
@@ -2,7 +2,7 @@ VALGRIND = valgrind -q --error-exitcode=127 --track-fds=yes --leak-check=full
 HELGRIND = valgrind -v --tool=helgrind
 
 check-valgrind: $(check_PROGRAMS)
-	$(MAKE) TESTS_ENVIRONMENT="$(VALGRIND)" check
+	$(MAKE) check LOG_COMPILE="libtool --mode=execute $(VALGRIND)"
 
 check-helgrind: $(check_PROGRAMS)
-	$(MAKE) TESTS_ENVIRONMENT="$(HELGRIND)" check
+	$(MAKE) check LOG_COMPILE="libtool --mode=execute $(HELGRIND)"
