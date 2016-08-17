ff80c90ae4c786fa2563d2e035d6376ce3743b22
Fabio M. Di Nitto <fdinitto@redhat.com>
2016-08-17 10:38:47 +0200

    [build] separate memcheck and helgrind suppressions files
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- build-aux/check.mk
+++ build-aux/check.mk
@@ -1,7 +1,7 @@
-VALGRIND = valgrind -q --error-exitcode=127 --suppressions=$(abs_top_srcdir)/build-aux/knet_valgrind.supp
+VALGRIND = valgrind -q --error-exitcode=127
 
-MEMCHECK = $(VALGRIND) --track-fds=yes --leak-check=full
-HELGRIND = $(VALGRIND) --tool=helgrind
+MEMCHECK = $(VALGRIND) --track-fds=yes --leak-check=full --suppressions=$(abs_top_srcdir)/build-aux/knet_valgrind_memcheck.supp
+HELGRIND = $(VALGRIND) --tool=helgrind --suppressions=$(abs_top_srcdir)/build-aux/knet_valgrind_helgrind.supp
 
 check-memcheck: $(check_PROGRAMS)
 	$(MAKE) check LOG_COMPILE="libtool --mode=execute $(MEMCHECK)"
