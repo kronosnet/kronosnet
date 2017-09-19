0678b5c2785ee4dbc30e6e4d07b3346e723c28ad
Fabio M. Di Nitto <fdinitto@redhat.com>
2017-09-19 21:03:26 +0200

    [crypto] add support for openssl1.0.2 and openssl1.1.0 to accomodate Debian unstable
    Please note that openssl1.1.0 needs more testing
    
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- libknet/crypto_openssl.c
+++ libknet/crypto_openssl.c
@@ -23,9 +23,18 @@
 #include "crypto_openssl.h"
 #include "logging.h"
 
-#define SSLERR_BUF_SIZE 256 /* https://www.openssl.org/docs/man1.0.2/crypto/ERR_error_string.html */
+/*
+ * 1.0.2 requires at least 120 bytes
+ * 1.1.0 requires at least 256 bytes
+ */
+#define SSLERR_BUF_SIZE 512
 
-#define LIBOPENSSL_10 "libcrypto.so.10"
+/*
+ * make this more generic.
+ * Fedora packages it one way, Debian another
+ * and it changes by version
+ */
+#define LIBOPENSSL "libcrypto.so"
 
 /*
  * global vars for dlopen
@@ -35,12 +44,21 @@ static void *openssl_lib;
 /*
  * symbols remapping
  */
+#ifdef BUILDCRYPTOOPENSSL10
 void (*_int_OPENSSL_add_all_algorithms_noconf)(void);
+#endif
+#ifdef BUILDCRYPTOOPENSSL11
+int (*_int_OPENSSL_init_crypto)(uint64_t opts, const OPENSSL_INIT_SETTINGS *settings);
+#endif
 
+#ifdef BUILDCRYPTOOPENSSL10
 void (*_int_ERR_load_crypto_strings)(void);
+#endif
 unsigned long (*_int_ERR_get_error)(void);
 void (*_int_ERR_error_string_n)(unsigned long e, char *buf, size_t len);
+#ifdef BUILDCRYPTOOPENSSL10
 void (*_int_ERR_free_strings)(void);
+#endif
 
 void (*_int_RAND_seed)(const void *buf, int num);
 int (*_int_RAND_bytes)(unsigned char *buf, int num);
@@ -53,8 +71,15 @@ unsigned char *(*_int_HMAC)(const EVP_MD *evp_md, const void *key, int key_len,
 
 const EVP_CIPHER *(*_int_EVP_get_cipherbyname)(const char *name);
 int (*_int_EVP_CIPHER_block_size)(const EVP_CIPHER *cipher);
+
+#ifdef BUILDCRYPTOOPENSSL10
 void (*_int_EVP_CIPHER_CTX_init)(EVP_CIPHER_CTX *a);
 int (*_int_EVP_CIPHER_CTX_cleanup)(EVP_CIPHER_CTX *a);
+#endif
+#ifdef BUILDCRYPTOOPENSSL11
+EVP_CIPHER_CTX *(*_int_EVP_CIPHER_CTX_new)(void);
+void (*_int_EVP_CIPHER_CTX_free)(EVP_CIPHER_CTX *c);
+#endif
 
 int (*_int_EVP_EncryptInit_ex)(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *cipher,
 			       ENGINE *impl, const unsigned char *key,
@@ -70,15 +95,26 @@ int (*_int_EVP_DecryptUpdate)(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl
 			      const unsigned char *in, int inl);
 int (*_int_EVP_DecryptFinal_ex)(EVP_CIPHER_CTX *ctx, unsigned char *outm, int *outl);
 
+#ifdef BUILDCRYPTOOPENSSL10
 void (*_int_EVP_cleanup)(void);
+#endif
 
 static void clean_openssl_syms(void)
 {
+#ifdef BUILDCRYPTOOPENSSL10
 	_int_OPENSSL_add_all_algorithms_noconf = NULL;
+#endif
+#ifdef BUILDCRYPTOOPENSSL11
+	_int_OPENSSL_init_crypto = NULL;
+#endif
+#ifdef BUILDCRYPTOOPENSSL10
 	_int_ERR_load_crypto_strings = NULL;
+#endif
 	_int_ERR_get_error = NULL;
 	_int_ERR_error_string_n = NULL;
+#ifdef BUILDCRYPTOOPENSSL10
 	_int_ERR_free_strings = NULL;
+#endif
 	_int_RAND_seed = NULL;
 	_int_RAND_bytes = NULL;
 	_int_EVP_get_digestbyname = NULL;
@@ -86,15 +122,23 @@ static void clean_openssl_syms(void)
 	_int_HMAC = NULL;
 	_int_EVP_get_cipherbyname = NULL;
 	_int_EVP_CIPHER_block_size = NULL;
+#ifdef BUILDCRYPTOOPENSSL10
 	_int_EVP_CIPHER_CTX_init = NULL;
 	_int_EVP_CIPHER_CTX_cleanup = NULL;
+#endif
+#ifdef BUILDCRYPTOOPENSSL11
+	_int_EVP_CIPHER_CTX_new = NULL;
+	_int_EVP_CIPHER_CTX_free = NULL;
+#endif
 	_int_EVP_EncryptInit_ex = NULL;
 	_int_EVP_EncryptUpdate = NULL;
 	_int_EVP_EncryptFinal_ex = NULL;
 	_int_EVP_DecryptInit_ex = NULL;
 	_int_EVP_DecryptUpdate = NULL;
 	_int_EVP_DecryptFinal_ex = NULL;
+#ifdef BUILDCRYPTOOPENSSL10
 	_int_EVP_cleanup = NULL;
+#endif
 	return;
 }
 
@@ -103,6 +147,7 @@ static int opensslcrypto_remap_symbols(knet_handle_t knet_h)
 	int err = 0;
 	char *error = NULL;
 
+#ifdef BUILDCRYPTOOPENSSL10
 	_int_OPENSSL_add_all_algorithms_noconf = dlsym(openssl_lib, "OPENSSL_add_all_algorithms_noconf");
 	if (!_int_OPENSSL_add_all_algorithms_noconf) {
 		error = dlerror();
@@ -110,7 +155,17 @@ static int opensslcrypto_remap_symbols(knet_handle_t knet_h)
 		err = -1;
 		goto out;
 	}
-
+#endif
+#ifdef BUILDCRYPTOOPENSSL11
+	_int_OPENSSL_init_crypto = dlsym(openssl_lib, "OPENSSL_init_crypto");
+	if (!_int_OPENSSL_init_crypto) {
+		error = dlerror();
+		log_err(knet_h, KNET_SUB_OPENSSLCRYPTO, "unable to map OPENSSL_init_crypto: %s", error);
+		err = -1;
+		goto out;
+	}
+#endif
+#ifdef BUILDCRYPTOOPENSSL10
 	_int_ERR_load_crypto_strings = dlsym(openssl_lib, "ERR_load_crypto_strings");
 	if (!_int_ERR_load_crypto_strings) {
 		error = dlerror();
@@ -118,6 +173,7 @@ static int opensslcrypto_remap_symbols(knet_handle_t knet_h)
 		err = -1;
 		goto out;
 	}
+#endif
 
 	_int_ERR_get_error = dlsym(openssl_lib, "ERR_get_error");
 	if (!_int_ERR_get_error) {
@@ -135,6 +191,7 @@ static int opensslcrypto_remap_symbols(knet_handle_t knet_h)
 		goto out;
 	}
 
+#ifdef BUILDCRYPTOOPENSSL10
 	_int_ERR_free_strings = dlsym(openssl_lib, "ERR_free_strings");
 	if (!_int_ERR_free_strings) {
 		error = dlerror();
@@ -142,6 +199,7 @@ static int opensslcrypto_remap_symbols(knet_handle_t knet_h)
 		err = -1;
 		goto out;
 	}
+#endif
 
 	_int_RAND_seed = dlsym(openssl_lib, "RAND_seed");
 	if (!_int_RAND_seed) {
@@ -199,6 +257,7 @@ static int opensslcrypto_remap_symbols(knet_handle_t knet_h)
 		goto out;
 	}
 
+#ifdef BUILDCRYPTOOPENSSL10
 	_int_EVP_CIPHER_CTX_init = dlsym(openssl_lib, "EVP_CIPHER_CTX_init");
 	if (!_int_EVP_CIPHER_CTX_init) {
 		error = dlerror();
@@ -214,6 +273,24 @@ static int opensslcrypto_remap_symbols(knet_handle_t knet_h)
 		err = -1;
 		goto out;
 	}
+#endif
+#ifdef BUILDCRYPTOOPENSSL11
+	_int_EVP_CIPHER_CTX_new = dlsym(openssl_lib, "EVP_CIPHER_CTX_new");
+	if (!_int_EVP_CIPHER_CTX_new) {
+		error = dlerror();
+		log_err(knet_h, KNET_SUB_OPENSSLCRYPTO, "unable to map EVP_CIPHER_CTX_new: %s", error);
+		err = -1;
+		goto out;
+	}
+
+	_int_EVP_CIPHER_CTX_free = dlsym(openssl_lib, "EVP_CIPHER_CTX_free");
+	if (!_int_EVP_CIPHER_CTX_free) {
+		error = dlerror();
+		log_err(knet_h, KNET_SUB_OPENSSLCRYPTO, "unable to map EVP_CIPHER_CTX_free: %s", error);
+		err = -1;
+		goto out;
+	}
+#endif
 
 	_int_EVP_EncryptInit_ex = dlsym(openssl_lib, "EVP_EncryptInit_ex");
 	if (!_int_EVP_EncryptInit_ex) {
@@ -263,6 +340,7 @@ static int opensslcrypto_remap_symbols(knet_handle_t knet_h)
 		goto out;
 	}
 
+#ifdef BUILDCRYPTOOPENSSL10
 	_int_EVP_cleanup = dlsym(openssl_lib, "EVP_cleanup");
 	if (!_int_EVP_cleanup) {
 		error = dlerror();
@@ -270,6 +348,7 @@ static int opensslcrypto_remap_symbols(knet_handle_t knet_h)
 		err = -1;
 		goto out;
 	}
+#endif
 
 out:
 	if (err) {
@@ -283,13 +362,14 @@ void opensslcrypto_unload_lib(
 	knet_handle_t knet_h)
 {
 	if (openssl_lib) {
+#ifdef BUILDCRYPTOOPENSSL10
 		if (_int_EVP_cleanup) {
 			(*_int_EVP_cleanup)();
 		}
 		if (_int_ERR_free_strings) {
 			(*_int_ERR_free_strings)();
 		}
-
+#endif
 		dlclose(openssl_lib);
 		openssl_lib = NULL;
 		clean_openssl_syms();
@@ -304,7 +384,7 @@ int opensslcrypto_load_lib(
 	int err = 0, savederrno = 0;
 
 	if (!openssl_lib) {
-		openssl_lib = open_lib(knet_h, LIBOPENSSL_10, 0);
+		openssl_lib = open_lib(knet_h, LIBOPENSSL, 0);
 		if (!openssl_lib) {
 			savederrno = errno;
 			err = -1;
@@ -317,8 +397,14 @@ int opensslcrypto_load_lib(
 			goto out;
 		}
 
+#ifdef BUILDCRYPTOOPENSSL10
 		(*_int_ERR_load_crypto_strings)();
 		(*_int_OPENSSL_add_all_algorithms_noconf)();
+#endif
+#ifdef BUILDCRYPTOOPENSSL11
+		(*_int_OPENSSL_init_crypto)(OPENSSL_INIT_ADD_ALL_CIPHERS \
+					    | OPENSSL_INIT_ADD_ALL_DIGESTS, NULL);
+#endif
 	}
 
 out:
@@ -346,9 +432,10 @@ struct opensslcrypto_instance {
 };
 
 /*
- * crypt/decrypt functions
+ * crypt/decrypt functions openssl1.0
  */
 
+#ifdef BUILDCRYPTOOPENSSL10
 static int encrypt_openssl(
 	knet_handle_t knet_h,
 	const struct iovec *iov,
@@ -458,6 +545,119 @@ out:
 	(*_int_EVP_CIPHER_CTX_cleanup)(&ctx);
 	return err;
 }
+#endif
+
+#ifdef BUILDCRYPTOOPENSSL11
+static int encrypt_openssl(
+	knet_handle_t knet_h,
+	const struct iovec *iov,
+	int iovcnt,
+	unsigned char *buf_out,
+	ssize_t *buf_out_len)
+{
+	struct opensslcrypto_instance *instance = knet_h->crypto_instance->model_instance;
+	EVP_CIPHER_CTX	*ctx;
+	int		tmplen = 0, offset = 0;
+	unsigned char	*salt = buf_out;
+	unsigned char	*data = buf_out + SALT_SIZE;
+	int		err = 0;
+	int		i;
+	char		sslerr[SSLERR_BUF_SIZE];
+
+	ctx = (*_int_EVP_CIPHER_CTX_new)();
+
+	/*
+	 * contribute to PRNG for each packet we send/receive
+	 */
+	(*_int_RAND_seed)((unsigned char *)iov[iovcnt - 1].iov_base, iov[iovcnt - 1].iov_len);
+
+	if (!(*_int_RAND_bytes)(salt, SALT_SIZE)) {
+		(*_int_ERR_error_string_n)((*_int_ERR_get_error)(), sslerr, sizeof(sslerr));
+		log_err(knet_h, KNET_SUB_OPENSSLCRYPTO, "Unable to get random salt data: %s", sslerr);
+		err = -1;
+		goto out;
+	}
+
+	/*
+	 * add warning re keylength
+	 */
+	(*_int_EVP_EncryptInit_ex)(ctx, instance->crypto_cipher_type, NULL, instance->private_key, salt);
+
+	for (i=0; i<iovcnt; i++) {
+		if (!(*_int_EVP_EncryptUpdate)(ctx,
+					       data + offset, &tmplen,
+					       (unsigned char *)iov[i].iov_base, iov[i].iov_len)) {
+			(*_int_ERR_error_string_n)((*_int_ERR_get_error)(), sslerr, sizeof(sslerr));
+			log_err(knet_h, KNET_SUB_OPENSSLCRYPTO, "Unable to encrypt: %s", sslerr);
+			err = -1;
+			goto out;
+		}
+		offset = offset + tmplen;
+	}
+
+	if (!(*_int_EVP_EncryptFinal_ex)(ctx, data + offset, &tmplen)) {
+		(*_int_ERR_error_string_n)((*_int_ERR_get_error)(), sslerr, sizeof(sslerr));
+		log_err(knet_h, KNET_SUB_OPENSSLCRYPTO, "Unable to finalize encrypt: %s", sslerr);
+		err = -1;
+		goto out;
+	}
+
+	*buf_out_len = offset + tmplen + SALT_SIZE;
+
+out:
+	(*_int_EVP_CIPHER_CTX_free)(ctx);
+	return err;
+}
+
+static int decrypt_openssl (
+	knet_handle_t knet_h,
+	const unsigned char *buf_in,
+	const ssize_t buf_in_len,
+	unsigned char *buf_out,
+	ssize_t *buf_out_len)
+{
+	struct opensslcrypto_instance *instance = knet_h->crypto_instance->model_instance;
+	EVP_CIPHER_CTX	*ctx;
+	int		tmplen1 = 0, tmplen2 = 0;
+	unsigned char	*salt = (unsigned char *)buf_in;
+	unsigned char	*data = salt + SALT_SIZE;
+	int		datalen = buf_in_len - SALT_SIZE;
+	int		err = 0;
+	char		sslerr[SSLERR_BUF_SIZE];
+
+	ctx = (*_int_EVP_CIPHER_CTX_new)();
+
+	/*
+	 * contribute to PRNG for each packet we send/receive
+	 */
+	(*_int_RAND_seed)(buf_in, buf_in_len);
+
+	/*
+	 * add warning re keylength
+	 */
+	(*_int_EVP_DecryptInit_ex)(ctx, instance->crypto_cipher_type, NULL, instance->private_key, salt);
+
+	if (!(*_int_EVP_DecryptUpdate)(ctx, buf_out, &tmplen1, data, datalen)) {
+		(*_int_ERR_error_string_n)((*_int_ERR_get_error)(), sslerr, sizeof(sslerr));
+		log_err(knet_h, KNET_SUB_OPENSSLCRYPTO, "Unable to decrypt: %s", sslerr);
+		err = -1;
+		goto out;
+	}
+
+	if (!(*_int_EVP_DecryptFinal_ex)(ctx, buf_out + tmplen1, &tmplen2)) {
+		(*_int_ERR_error_string_n)((*_int_ERR_get_error)(), sslerr, sizeof(sslerr));
+		log_err(knet_h, KNET_SUB_OPENSSLCRYPTO, "Unable to finalize decrypt: %s", sslerr);
+		err = -1;
+		goto out;
+	}
+
+	*buf_out_len = tmplen1 + tmplen2;
+
+out:
+	(*_int_EVP_CIPHER_CTX_free)(ctx);
+	return err;
+}
+#endif
 
 /*
  * hash/hmac/digest functions
