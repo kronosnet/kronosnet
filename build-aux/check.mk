3e776e82ddfce76ea437108a6511855021e491c5
Fabio M. Di Nitto <fdinitto@redhat.com>
2017-10-15 10:32:50 +0200

    [build] allow make check-memcheck and check-hellgrind only if valgrind is avalable
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- build-aux/check.mk
+++ build-aux/check.mk
@@ -1,12 +1,20 @@
-VALGRIND = valgrind -q --error-exitcode=127
+VALGRIND = $(VALGRIND_EXEC) -q --error-exitcode=127
 
 MEMCHECK = $(VALGRIND) --track-fds=yes --leak-check=full --suppressions=$(abs_top_srcdir)/build-aux/knet_valgrind_memcheck.supp
 HELGRIND = $(VALGRIND) --tool=helgrind --suppressions=$(abs_top_srcdir)/build-aux/knet_valgrind_helgrind.supp
 
 check-memcheck: $(check_PROGRAMS)
+if HAS_VALGRIND
 	export KNETMEMCHECK=yes && \
 		$(MAKE) check LOG_COMPILE="libtool --mode=execute $(MEMCHECK)"
+else
+	@echo valgrind not available on this platform
+endif
 
 check-helgrind: $(check_PROGRAMS)
+if HAS_VALGRIND
 	export KNETHELGRIND=yes && \
 		$(MAKE) check LOG_COMPILE="libtool --mode=execute $(HELGRIND)"
+else
+	@echo valgrind not available on this platform
+endif
