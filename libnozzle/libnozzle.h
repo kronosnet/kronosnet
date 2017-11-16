a14a55cd0168888ba9e085277fd761864e998619
Fabio M. Di Nitto <fdinitto@redhat.com>
2017-11-16 13:52:08 +0100

    [nozzle] rename API from tap_ to nozzle_
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- libnozzle/libnozzle.h
+++ libnozzle/libnozzle.h
@@ -23,28 +23,28 @@
 
 typedef struct nozzle_iface *nozzle_t;
 
-nozzle_t tap_open(char *dev, size_t dev_size, const char *updownpath);
-int tap_close(nozzle_t tap);
+nozzle_t nozzle_open(char *dev, size_t dev_size, const char *updownpath);
+int nozzle_close(nozzle_t tap);
 
-nozzle_t tap_find(char *dev, size_t dev_size);
+nozzle_t nozzle_find(char *dev, size_t dev_size);
 
-int tap_get_fd(const nozzle_t tap);
+int nozzle_get_fd(const nozzle_t tap);
 
-const char *tap_get_name(const nozzle_t tap);
+const char *nozzle_get_name(const nozzle_t tap);
 
-int tap_get_mtu(const nozzle_t tap);
-int tap_set_mtu(nozzle_t tap, const int mtu);
-int tap_reset_mtu(nozzle_t tap);
+int nozzle_get_mtu(const nozzle_t tap);
+int nozzle_set_mtu(nozzle_t tap, const int mtu);
+int nozzle_reset_mtu(nozzle_t tap);
 
-int tap_get_mac(const nozzle_t tap, char **ether_addr);
-int tap_set_mac(nozzle_t tap, const char *ether_addr);
-int tap_reset_mac(nozzle_t tap);
+int nozzle_get_mac(const nozzle_t tap, char **ether_addr);
+int nozzle_set_mac(nozzle_t tap, const char *ether_addr);
+int nozzle_reset_mac(nozzle_t tap);
 
-int tap_set_up(nozzle_t tap, char **error_preup, char **error_up);
-int tap_set_down(nozzle_t tap, char **error_down, char **error_postdown);
+int nozzle_set_up(nozzle_t tap, char **error_preup, char **error_up);
+int nozzle_set_down(nozzle_t tap, char **error_down, char **error_postdown);
 
-int tap_add_ip(nozzle_t tap, const char *ip_addr, const char *prefix, char **error_string);
-int tap_del_ip(nozzle_t tap, const char *ip_addr, const char *prefix, char **error_string);
-int tap_get_ips(const nozzle_t tap, char **ip_addr_list, int *entries);
+int nozzle_add_ip(nozzle_t tap, const char *ip_addr, const char *prefix, char **error_string);
+int nozzle_del_ip(nozzle_t tap, const char *ip_addr, const char *prefix, char **error_string);
+int nozzle_get_ips(const nozzle_t tap, char **ip_addr_list, int *entries);
 
 #endif
