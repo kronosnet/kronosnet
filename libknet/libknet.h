f641ba8562b43c184911f6ef93f56fcca51f369c
Fabio M. Di Nitto <fdinitto@redhat.com>
2017-11-23 11:39:01 +0100

    [links] extend knet_link_set_ping_timers ABI to be ready for future expansion
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- libknet/libknet.h
+++ libknet/libknet.h
@@ -1467,6 +1467,12 @@ int knet_link_get_enable(knet_handle_t knet_h, knet_node_id_t host_id, uint8_t l
  *
  * timeout   - if no pong is received within this time,
  *             the link is declared dead, in milliseconds.
+ *             NOTE: in future it will be possible to set timeout to 0
+ *             for an autocalculated timeout based on interval, pong_count
+ *             and latency. The API already accept 0 as value and it will
+ *             return ENOSYS / -1. Once the automatic calculation feature
+ *             will be implemented, this call will only return EINVAL
+ *             for incorrect values.
  *
  * precision - how many values of latency are used to calculate
  *             the average link latency (see also knet_link_get_status(3))
