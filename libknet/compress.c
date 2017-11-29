8cec059babc4967bdb570fd7002681c97240c0a0
Fabio M. Di Nitto <fdinitto@redhat.com>
2017-11-29 09:03:08 +0100

    [modules] drop redundant check
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- libknet/compress.c
+++ libknet/compress.c
@@ -94,14 +94,13 @@ static int load_compress_lib(knet_handle_t knet_h, compress_model_t *model)
 	char soname[MAXPATHLEN];
 	const char model_sym[] = "compress_model";
 
-	if (model->loaded) {
-		return 0;
-	}
 	snprintf (soname, sizeof soname, "compress_%s.so", model->model_name);
+
 	module = open_lib(knet_h, soname, 0);
 	if (!module) {
 		return -1;
 	}
+
 	module_cmds = dlsym (module, model_sym);
 	if (!module_cmds) {
 		log_err (knet_h, KNET_SUB_COMPRESS, "unable to map symbol %s in module %s: %s",
@@ -109,12 +108,14 @@ static int load_compress_lib(knet_handle_t knet_h, compress_model_t *model)
 		errno = EINVAL;
 		return -1;
 	}
+
 	model->is_init = module_cmds->is_init;
 	model->init = module_cmds->init;
 	model->fini = module_cmds->fini;
 	model->val_level = module_cmds->val_level;
 	model->compress = module_cmds->compress;
 	model->decompress = module_cmds->decompress;
+
 	return 0;
 }
 
