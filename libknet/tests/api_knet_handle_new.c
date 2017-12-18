1fcf7db2e51511297461f588ebd0361b0a4ce70e
Ferenc Wágner <wferi@debian.org>
2017-12-18 21:31:45 +0100

    tests: replace open coded error handling with knet_handle_start() calls
    Done by:
    $ sed -i -e '1h;2,$H;$!d;g' -e 's/knet_h = knet_handle_new(1, logfds\[1\], \(KNET_LOG_[A-Z]*\));\n\n\tif (!knet_h) {\n\t\tprintf("knet_handle_new failed: %s\\n", strerror(errno));\n\t\tflush_logs(logfds\[0\], stdout);\n\t\tclose_logpipes(logfds);\n\t\texit(FAIL);\n\t}/knet_h = knet_handle_start(logfds, \1);/g' libknet/tests/*.c
    
    Signed-off-by: Ferenc Wágner <wferi@debian.org>

--- libknet/tests/api_knet_handle_new.c
+++ libknet/tests/api_knet_handle_new.c
@@ -115,14 +115,7 @@ static void test(void)
 
 	printf("Test knet_handle_new hostid 1, proper log_fd, proper log level (DEBUG)\n");
 
-	knet_h = knet_handle_new(1, logfds[1], KNET_LOG_DEBUG);
-
-	if (!knet_h) {
-		printf("knet_handle_new failed: %s\n", strerror(errno));
-		flush_logs(logfds[0], stdout);
-		close_logpipes(logfds);
-		exit(FAIL);
-	}
+	knet_h = knet_handle_start(logfds, KNET_LOG_DEBUG);
 
 	knet_handle_free(knet_h);
 	flush_logs(logfds[0], stdout);
