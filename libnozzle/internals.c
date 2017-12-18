d8547cec10612ca88bc91bee8ad7d3f073b226c2
Fabio M. Di Nitto <fdinitto@redhat.com>
2017-12-18 07:17:39 +0100

    [nozzle] split code around to separate libnozzle and testsuite
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- libnozzle/internals.c
+++ libnozzle/internals.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2018 Red Hat, Inc.  All rights reserved.
+ * Copyright (C) 2010-2017 Red Hat, Inc.  All rights reserved.
  *
  * Author: Fabio M. Di Nitto <fabbione@kronosnet.org>
  *
@@ -13,50 +13,154 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/socket.h>
 #include <sys/wait.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
-#include <net/ethernet.h>
-#include <arpa/inet.h>
-#include <pthread.h>
 #include <limits.h>
 #include <stdio.h>
-#include <net/if.h>
-#include <ifaddrs.h>
 #include <stdint.h>
-
-#ifdef KNET_LINUX
-#include <linux/if_tun.h>
-#include <netinet/ether.h>
-#endif
-#ifdef KNET_BSD
-#include <net/if_dl.h>
-#endif
 
 #include "libnozzle.h"
 #include "internals.h"
 
-static int lib_init = 0;
-static struct nozzle_lib_config lib_cfg;
-static pthread_mutex_t config_mutex = PTHREAD_MUTEX_INITIALIZER;
+static int _read_pipe(int fd, char **file, size_t *length)
+{
+	char buf[4096];
+	int n;
+	int done = 0;
 
-/* forward declarations */
-static int _check(const nozzle_t nozzle);
-static void _close(nozzle_t nozzle);
-static void _close_cfg(void);
-static int _get_mtu(const nozzle_t nozzle);
-static int _get_mac(const nozzle_t nozzle, char **ether_addr);
-static int _set_down(nozzle_t nozzle, char **error_down, char **error_postdown);
-static char *_get_v4_broadcast(const char *ipaddr, const char *prefix);
-static int _set_ip(nozzle_t nozzle, const char *command,
-		      const char *ipaddr, const char *prefix,
-		      char **error_string, int secondary);
-static int _find_ip(nozzle_t nozzle,
-			const char *ipaddr, const char *prefix,
-			struct nozzle_ip **ip, struct nozzle_ip **ip_prev);
+	*file = NULL;
+	*length = 0;
 
+	memset(buf, 0, sizeof(buf));
+
+	while (!done) {
+
+		n = read(fd, buf, sizeof(buf));
+
+		if (n < 0) {
+			if (errno == EINTR)
+				continue;
+
+			if (*file)
+				free(*file);
+
+			return n;
+		}
+
+		if (n == 0 && (!*length))
+			return 0;
+
+		if (n == 0)
+			done = 1;
+
+		if (*file)
+			*file = realloc(*file, (*length) + n + done);
+		else
+			*file = malloc(n + done);
+
+		if (!*file)
+			return -1;
+
+		memmove((*file) + (*length), buf, n);
+		*length += (done + n);
+	}
+
+	/* Null terminator */
+	(*file)[(*length) - 1] = 0;
+
+	return 0;
+}
+
+int _execute_shell(const char *command, char **error_string)
+{
+	pid_t pid;
+	int status, err = 0;
+	int fd[2];
+	size_t size = 0;
+
+	if ((command == NULL) || (!error_string)) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	*error_string = NULL;
+
+	err = pipe(fd);
+	if (err)
+		goto out_clean;
+
+	pid = fork();
+	if (pid < 0) {
+		err = pid;
+		goto out_clean;
+	}
+
+	if (pid) { /* parent */
+
+		close(fd[1]);
+		err = _read_pipe(fd[0], error_string, &size);
+		if (err)
+			goto out_clean0;
+
+		waitpid(pid, &status, 0);
+		if (!WIFEXITED(status)) {
+			err = -1;
+			goto out_clean0;
+		}
+		if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+			err = WEXITSTATUS(status);
+			goto out_clean0;
+		}
+		goto out_clean0;
+	} else { /* child */
+		close(0);
+		close(1);
+		close(2);
+
+		close(fd[0]);
+		dup2(fd[1], 1);
+		dup2(fd[1], 2);
+		close(fd[1]);
+
+		execlp("/bin/sh", "/bin/sh", "-c", command, NULL);
+		exit(EXIT_FAILURE);
+	}
+
+out_clean:
+	close(fd[1]);
+out_clean0:
+	close(fd[0]);
+
+	return err;
+}
+
+int _exec_updown(const nozzle_t nozzle, const char *action, char **error_string)
+{
+	char command[PATH_MAX];
+	struct stat sb;
+	int err = 0;
+
+	if (!nozzle->hasupdown)
+		return 0;
+
+	memset(command, 0, PATH_MAX);
+
+	snprintf(command, PATH_MAX, "%s%s/%s", nozzle->updownpath, action, nozzle->name);
+
+	err = stat(command, &sb);
+	if ((err < 0) && (errno == ENOENT))
+		return 0;
+
+	err = _execute_shell(command, error_string);
+	if ((!err) && (*error_string)) {
+		free(*error_string);
+		*error_string = NULL;
+	}
+
+	return err;
+}
+
+#if 0
 static int _check(const nozzle_t nozzle)
 {
 	nozzle_t temp = lib_cfg.head;
@@ -1152,3 +1256,4 @@ out_clean:
 	errno = savederrno;
 	return err;
 }
+#endif
