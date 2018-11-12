kronosnet/kronosnet

commit 18ab48885e9e8cf9fadf117828e8fa0b2d20a77e

[transports] make KNET_MAX_TRANSPORTS = UINT8_MAX now that the APIs allow holes in the IDs
this way we don't need to break ABI if we add new transports later on.

Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

libknet/transports.c
@@ -159,7 +159,7 @@ const char *knet_get_transport_name_by_id(uint8_t transport)
 	int savederrno = 0;
 	const char *name = NULL;
 
-	if (transport >= KNET_MAX_TRANSPORTS) {
+	if (transport == KNET_MAX_TRANSPORTS) {
 		errno = EINVAL;
 		return name;
 	}

libknet/libknet.h
@@ -1183,7 +1183,7 @@ int knet_addrtostr(const struct sockaddr_storage *ss, socklen_t sslen,
 #define KNET_TRANSPORT_LOOPBACK 0
 #define KNET_TRANSPORT_UDP      1
 #define KNET_TRANSPORT_SCTP     2
-#define KNET_MAX_TRANSPORTS     3
+#define KNET_MAX_TRANSPORTS     UINT8_MAX
 
 /*
  * The Loopback transport is only valid for connections to localhost, the host

libknet/tests/api_knet_get_transport_list.c
@@ -23,7 +23,7 @@ static void test(void)
 {
 	struct knet_transport_info transport_list[KNET_MAX_TRANSPORTS];
 	size_t transport_list_entries;
-	size_t i, expected_count;
+	size_t i;
 
 	memset(transport_list, 0, sizeof(transport_list));
 
@@ -51,16 +51,6 @@ static void test(void)
 	for (i=0; i<transport_list_entries; i++) {
 		printf("Detected transport: %s id: %d properties: %u\n",
 			transport_list[i].name, transport_list[i].id, transport_list[i].properties);
-	}
-
-	expected_count = KNET_MAX_TRANSPORTS;
-#ifndef HAVE_NETINET_SCTP_H
-	expected_count--;
-#endif
-
-	if (transport_list_entries != expected_count) {
-		printf("Error! expected: %zu transports, got: %zu\n", expected_count, transport_list_entries);
-		exit(FAIL);
 	}
 }
 

libknet/tests/api_knet_link_set_config.c
@@ -74,7 +74,7 @@ static void test(void)
 
 	printf("Test knet_link_set_config with bad transport type\n");
 
-	if ((!knet_link_set_config(knet_h, 1, 0, KNET_MAX_TRANSPORTS+1, &src, &dst, 0)) || (errno != EINVAL)) {
+	if ((!knet_link_set_config(knet_h, 1, 0, KNET_MAX_TRANSPORTS, &src, &dst, 0)) || (errno != EINVAL)) {
 		printf("knet_link_set_config accepted invalid transport or returned incorrect error: %s\n", strerror(errno));
 		knet_handle_free(knet_h);
 		flush_logs(logfds[0], stdout);

commit bd6baddef7183d89911d86607239c391cbde6c79

[transports] fix transport_modules_cmd array out of range
Fixes: 18ab4888

Signed-off-by: Chen Jingpiao <jchen@suse.com>

libknet/transports.c
@@ -29,7 +29,7 @@
 
 #define empty_module 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL },
 
-knet_transport_ops_t transport_modules_cmd[] = {
+knet_transport_ops_t transport_modules_cmd[KNET_MAX_TRANSPORTS] = {
 	{ "LOOPBACK", KNET_TRANSPORT_LOOPBACK, 1, KNET_PMTUD_LOOPBACK_OVERHEAD, loopback_transport_init, loopback_transport_free, loopback_transport_link_set_config, loopback_transport_link_clear_config, loopback_transport_link_dyn_connect, loopback_transport_rx_sock_error, loopback_transport_tx_sock_error, loopback_transport_rx_is_data },
 	{ "UDP", KNET_TRANSPORT_UDP, 1, KNET_PMTUD_UDP_OVERHEAD, udp_transport_init, udp_transport_free, udp_transport_link_set_config, udp_transport_link_clear_config, udp_transport_link_dyn_connect, udp_transport_rx_sock_error, udp_transport_tx_sock_error, udp_transport_rx_is_data },
 	{ "SCTP", KNET_TRANSPORT_SCTP,