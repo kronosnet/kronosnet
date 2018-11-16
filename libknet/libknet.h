kronosnet/kronosnet

commit aff0a34619d3cdaf84e32e7290a43f8c25f22930

[global] revert b525ee6a6b74ec, go back to 64K nodes, and use typedef for nodeid
Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

libknet/libknet.h
@@ -21,7 +21,8 @@
  * Maximum number of hosts
  */
 
-#define KNET_MAX_HOST 256
+typedef uint16_t knet_node_id_t;
+#define KNET_MAX_HOST 65536
 
 /*
  * Maximum number of links between 2 hosts
@@ -83,7 +84,7 @@ typedef struct knet_handle *knet_handle_t;
  * on failure, NULL is returned and errno is set.
  */
 
-knet_handle_t knet_handle_new(uint8_t  host_id,
+knet_handle_t knet_handle_new(knet_node_id_t host_id,
 			      int      log_fd,
 			      uint8_t  default_log_level);
 
@@ -379,10 +380,10 @@ int knet_send_sync(knet_handle_t knet_h,
  *            ssize_t outdata_len          - lenght of the above data
  *            uint8_t tx_rx                - filter is called on tx or rx
  *                                           (see defines below)
- *            uint8_t this_host_id        - host_id processing the packet
- *            uint8_t src_host_id         - host_id that generated the
+ *            knet_node_id_t this_host_id  - host_id processing the packet
+ *            knet_node_id_t src_host_id   - host_id that generated the
  *                                           packet
- *            uint8_t *dst_host_ids        - array of KNET_MAX_HOST uint8_t
+ *            knet_node_id_t *dst_host_ids - array of KNET_MAX_HOST knet_node_id_t
  *                                           where to store the destinations
  *            size_t *dst_host_ids_entries - number of hosts to send the message
  *
@@ -408,10 +409,10 @@ int knet_handle_enable_filter(knet_handle_t knet_h,
 					const unsigned char *outdata,
 					ssize_t outdata_len,
 					uint8_t tx_rx,
-					uint8_t this_host_id,
-					uint8_t src_host_id,
+					knet_node_id_t this_host_id,
+					knet_node_id_t src_host_id,
 					int8_t *channel,
-					uint8_t *dst_host_ids,
+					knet_node_id_t *dst_host_ids,
 					size_t *dst_host_ids_entries));
 
 /*
@@ -607,7 +608,7 @@ int knet_handle_crypto(knet_handle_t knet_h,
  * -1 on error and errno is set.
  */
 
-int knet_host_add(knet_handle_t knet_h, uint8_t host_id);
+int knet_host_add(knet_handle_t knet_h, knet_node_id_t host_id);
 
 /*
  * knet_host_remove
@@ -623,7 +624,7 @@ int knet_host_add(knet_handle_t knet_h, uint8_t host_id);
  * -1 on error and errno is set.
  */
 
-int knet_host_remove(knet_handle_t knet_h, uint8_t host_id);
+int knet_host_remove(knet_handle_t knet_h, knet_node_id_t host_id);
 
 /*
  * knet_host_set_name
@@ -643,7 +644,7 @@ int knet_host_remove(knet_handle_t knet_h, uint8_t host_id);
  * -1 on error and errno is set.
  */
 
-int knet_host_set_name(knet_handle_t knet_h, uint8_t host_id,
+int knet_host_set_name(knet_handle_t knet_h, knet_node_id_t host_id,
 		       const char *name);
 
 /*
@@ -663,7 +664,7 @@ int knet_host_set_name(knet_handle_t knet_h, uint8_t host_id,
  * -1 on error and errno is set (name is left untouched)
  */
 
-int knet_host_get_name_by_host_id(knet_handle_t knet_h, uint8_t host_id,
+int knet_host_get_name_by_host_id(knet_handle_t knet_h, knet_node_id_t host_id,
 				  char *name);
 
 /*
@@ -682,7 +683,7 @@ int knet_host_get_name_by_host_id(knet_handle_t knet_h, uint8_t host_id,
  */
 
 int knet_host_get_id_by_host_name(knet_handle_t knet_h, const char *name,
-				  uint8_t *host_id);
+				  knet_node_id_t *host_id);
 
 /*
  * knet_host_get_host_list
@@ -701,7 +702,7 @@ int knet_host_get_id_by_host_name(knet_handle_t knet_h, const char *name,
  */
 
 int knet_host_get_host_list(knet_handle_t knet_h,
-			    uint8_t *host_ids, size_t *host_ids_entries);
+			    knet_node_id_t *host_ids, size_t *host_ids_entries);
 
 /*
  * define switching policies
@@ -740,7 +741,7 @@ int knet_host_get_host_list(knet_handle_t knet_h,
  * -1 on error and errno is set.
  */
 
-int knet_host_set_policy(knet_handle_t knet_h, uint8_t host_id,
+int knet_host_set_policy(knet_handle_t knet_h, knet_node_id_t host_id,
 			 uint8_t policy);
 
 /*
@@ -759,7 +760,7 @@ int knet_host_set_policy(knet_handle_t knet_h, uint8_t host_id,
  * -1 on error and errno is set.
  */
 
-int knet_host_get_policy(knet_handle_t knet_h, uint8_t host_id,
+int knet_host_get_policy(knet_handle_t knet_h, knet_node_id_t host_id,
 			 uint8_t *policy);
 
 /*
@@ -798,7 +799,7 @@ int knet_host_enable_status_change_notify(knet_handle_t knet_h,
 					  void *host_status_change_notify_fn_private_data,
 					  void (*host_status_change_notify_fn) (
 						void *private_data,
-						uint8_t host_id,
+						knet_node_id_t host_id,
 						uint8_t reachable,
 						uint8_t remote,
 						uint8_t external));
@@ -837,7 +838,7 @@ struct knet_host_status {
  * -1 on error and errno is set.
  */
 
-int knet_host_get_status(knet_handle_t knet_h, uint8_t host_id,
+int knet_host_get_status(knet_handle_t knet_h, knet_node_id_t host_id,
 			 struct knet_host_status *status);
 
 /*
@@ -994,7 +995,7 @@ uint8_t knet_handle_get_transport_id_by_name(knet_handle_t knet_h, const char *n
  * -1 on error and errno is set.
  */
 
-int knet_link_set_config(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
+int knet_link_set_config(knet_handle_t knet_h, knet_node_id_t host_id, uint8_t link_id,
 			 uint8_t transport,
 			 struct sockaddr_storage *src_addr,
 			 struct sockaddr_storage *dst_addr);
@@ -1024,7 +1025,7 @@ int knet_link_set_config(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
  * -1 on error and errno is set.
  */
 
-int knet_link_get_config(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
+int knet_link_get_config(knet_handle_t knet_h, knet_node_id_t host_id, uint8_t link_id,
 			 uint8_t *transport,
 			 struct sockaddr_storage *src_addr,
 			 struct sockaddr_storage *dst_addr,
@@ -1045,7 +1046,7 @@ int knet_link_get_config(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
  * -1 on error and errno is set.
  */
 
-int knet_link_clear_config(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id);
+int knet_link_clear_config(knet_handle_t knet_h, knet_node_id_t host_id, uint8_t link_id);
 
 /*
  * knet_link_set_enable
@@ -1064,7 +1065,7 @@ int knet_link_clear_config(knet_handle_t knet_h, uint8_t host_id, uint8_t link_i
  * -1 on error and errno is set.
  */
 
-int knet_link_set_enable(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
+int knet_link_set_enable(knet_handle_t knet_h, knet_node_id_t host_id, uint8_t link_id,
 			 unsigned int enabled);
 
 /*
@@ -1084,7 +1085,7 @@ int knet_link_set_enable(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
  * -1 on error and errno is set.
  */
 
-int knet_link_get_enable(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
+int knet_link_get_enable(knet_handle_t knet_h, knet_node_id_t host_id, uint8_t link_id,
 			 unsigned int *enabled);
 
 /*
@@ -1114,7 +1115,7 @@ int knet_link_get_enable(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
 #define KNET_LINK_DEFAULT_PING_TIMEOUT   2000 /* 2 seconds */
 #define KNET_LINK_DEFAULT_PING_PRECISION 2048 /* samples */
 
-int knet_link_set_ping_timers(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
+int knet_link_set_ping_timers(knet_handle_t knet_h, knet_node_id_t host_id, uint8_t link_id,
 			      time_t interval, time_t timeout, unsigned int precision);
 
 /*
@@ -1140,7 +1141,7 @@ int knet_link_set_ping_timers(knet_handle_t knet_h, uint8_t host_id, uint8_t lin
  * -1 on error and errno is set.
  */
 
-int knet_link_get_ping_timers(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
+int knet_link_get_ping_timers(knet_handle_t knet_h, knet_node_id_t host_id, uint8_t link_id,
 			      time_t *interval, time_t *timeout, unsigned int *precision);
 
 /*
@@ -1163,7 +1164,7 @@ int knet_link_get_ping_timers(knet_handle_t knet_h, uint8_t host_id, uint8_t lin
 
 #define KNET_LINK_DEFAULT_PONG_COUNT 5
 
-int knet_link_set_pong_count(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
+int knet_link_set_pong_count(knet_handle_t knet_h, knet_node_id_t host_id, uint8_t link_id,
 			     uint8_t pong_count);
 
 /*
@@ -1183,7 +1184,7 @@ int knet_link_set_pong_count(knet_handle_t knet_h, uint8_t host_id, uint8_t link
  * -1 on error and errno is set.
  */
 
-int knet_link_get_pong_count(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
+int knet_link_get_pong_count(knet_handle_t knet_h, knet_node_id_t host_id, uint8_t link_id,
 			     uint8_t *pong_count);
 
 /*
@@ -1204,7 +1205,7 @@ int knet_link_get_pong_count(knet_handle_t knet_h, uint8_t host_id, uint8_t link
  * -1 on error and errno is set.
  */
 
-int knet_link_set_priority(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
+int knet_link_set_priority(knet_handle_t knet_h, knet_node_id_t host_id, uint8_t link_id,
 			   uint8_t priority);
 
 /*
@@ -1225,7 +1226,7 @@ int knet_link_set_priority(knet_handle_t knet_h, uint8_t host_id, uint8_t link_i
  * -1 on error and errno is set.
  */
 
-int knet_link_get_priority(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
+int knet_link_get_priority(knet_handle_t knet_h, knet_node_id_t host_id, uint8_t link_id,
 			   uint8_t *priority);
 
 /*
@@ -1245,7 +1246,7 @@ int knet_link_get_priority(knet_handle_t knet_h, uint8_t host_id, uint8_t link_i
  * -1 on error and errno is set.
  */
 
-int knet_link_get_link_list(knet_handle_t knet_h, uint8_t host_id,
+int knet_link_get_link_list(knet_handle_t knet_h, knet_node_id_t host_id,
 			    uint8_t *link_ids, size_t *link_ids_entries);
 
 /*
@@ -1314,7 +1315,7 @@ struct knet_link_status {
  * -1 on error and errno is set.
  */
 
-int knet_link_get_status(knet_handle_t knet_h, uint8_t host_id, uint8_t link_id,
+int knet_link_get_status(knet_handle_t knet_h, knet_node_id_t host_id, uint8_t link_id,
 			 struct knet_link_status *status);
 
 /*

kronosnetd/cfg.h
@@ -21,7 +21,7 @@
 struct knet_cfg_eth {
 	tap_t tap;
 	int auto_mtu;
-	uint8_t node_id;
+	knet_node_id_t node_id;
 };
 
 struct knet_cfg_ring {

kronosnetd/etherfilter.c
@@ -37,10 +37,10 @@ int ether_host_filter_fn (void *private_data,
 			  const unsigned char *outdata,
 			  ssize_t outdata_len,
 			  uint8_t tx_rx,
-			  uint8_t this_host_id,
-			  uint8_t src_host_id,
+			  knet_node_id_t this_host_id,
+			  knet_node_id_t src_host_id,
 			  int8_t *channel,
-			  uint8_t *dst_host_ids,
+			  knet_node_id_t *dst_host_ids,
 			  size_t *dst_host_ids_entries)
 {
 	struct ether_header *eth_h = (struct ether_header *)outdata;

kronosnetd/etherfilter.h
@@ -10,15 +10,16 @@
 #define __ETHERFILTER_H__
 
 #include <stdint.h>
+#include "libknet.h"
 
 int ether_host_filter_fn (void *private_data,
 			  const unsigned char *outdata,
 			  ssize_t outdata_len,
 			  uint8_t tx_rx,
-			  uint8_t this_host_id,
-			  uint8_t src_host_id,
+			  knet_node_id_t this_host_id,
+			  knet_node_id_t src_host_id,
 			  int8_t *channel,
-			  uint8_t *dst_host_ids,
+			  knet_node_id_t *dst_host_ids,
 			  size_t *dst_host_ids_entries);
 
 #endif

kronosnetd/vty.h
@@ -13,6 +13,7 @@
 #include <pthread.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include "libknet.h"
 
 #define KNET_VTY_DEFAULT_PORT		50000
 
@@ -57,7 +58,7 @@ struct knet_vty {
 	int			logfd;		/* fd to pass to iface create */
 	int			loglevel;	/* loglevel (debug, etc) */
 	void			*iface;		/* pointer to iface we are working on */
-	uint8_t			host_id;	/* peer/host we are working on */
+	knet_node_id_t		host_id;	/* peer/host we are working on */
 	uint8_t			link_id;	/* link id we are working on */
 	int			filemode;	/* tell print_conf to add or not carriage return */
 	struct knet_vty_global_conf *vty_global_conf; /* pointer to vty global config */

kronosnetd/vty_cli_cmds.c
@@ -1071,11 +1071,11 @@ static int knet_cmd_switch_policy(struct knet_vty *vty)
  *  0 host does not exist
  *  1 host exists
  */
-static int knet_find_host(struct knet_vty *vty,	const char *nodename, const uint8_t requested_node_id)
+static int knet_find_host(struct knet_vty *vty,	const char *nodename, const knet_node_id_t requested_node_id)
 {
 	struct knet_cfg *knet_iface = (struct knet_cfg *)vty->iface;
 	int have_nodeid, have_name;
-	uint8_t node_id;
+	knet_node_id_t node_id;
 	char name[KNET_MAX_HOST_LEN];
 
 	have_nodeid = knet_host_get_id_by_host_name(knet_iface->cfg_ring.knet_h, nodename, &node_id);
@@ -1522,7 +1522,7 @@ static int knet_cmd_no_interface(struct knet_vty *vty)
 	char *ip_list = NULL;
 	int ip_list_entries = 0, j, i, offset = 0;
 	char *error_string = NULL;
-	uint8_t host_ids[KNET_MAX_HOST];
+	knet_node_id_t host_ids[KNET_MAX_HOST];
 	uint8_t link_ids[KNET_MAX_LINK];
 	size_t host_ids_entries = 0, link_ids_entries = 0;
 
@@ -1745,7 +1745,7 @@ static int knet_cmd_status(struct knet_vty *vty)
 	const char *nl = telnet_newline;
 	struct timespec now;
 	char nodename[KNET_MAX_HOST_LEN];
-	uint8_t host_ids[KNET_MAX_HOST];
+	knet_node_id_t host_ids[KNET_MAX_HOST];
 	uint8_t link_ids[KNET_MAX_LINK];
 	size_t host_ids_entries = 0, link_ids_entries = 0;
 	uint8_t policy;
@@ -1826,7 +1826,7 @@ static int knet_cmd_print_conf(struct knet_vty *vty)
 	const char *nl = telnet_newline;
 	char *ip_list = NULL;
 	int ip_list_entries = 0;
-	uint8_t host_ids[KNET_MAX_HOST];
+	knet_node_id_t host_ids[KNET_MAX_HOST];
 	uint8_t link_ids[KNET_MAX_LINK];
 	size_t host_ids_entries = 0, link_ids_entries = 0;
 	char nodename[KNET_MAX_HOST_LEN];

libknet/handle.c
@@ -505,9 +505,9 @@ static void _stop_threads(knet_handle_t knet_h)
 	}
 }
 
-knet_handle_t knet_handle_new(uint8_t host_id,
-			      int     log_fd,
-			      uint8_t default_log_level)
+knet_handle_t knet_handle_new(knet_node_id_t host_id,
+			      int	     log_fd,
+			      uint8_t	     default_log_level)
 {
 	knet_handle_t knet_h;
 	int savederrno = 0;
@@ -1033,10 +1033,10 @@ int knet_handle_enable_filter(knet_handle_t knet_h,
 					const unsigned char *outdata,
 					ssize_t outdata_len,
 					uint8_t tx_rx,
-					uint8_t this_host_id,
-					uint8_t src_node_id,
+					knet_node_id_t this_host_id,
+					knet_node_id_t src_node_id,
 					int8_t *channel,
-					uint8_t *dst_host_ids,
+					knet_node_id_t *dst_host_ids,
 					size_t *dst_host_ids_entries))
 {
 	int savederrno = 0;

libknet/host.c
@@ -30,7 +30,7 @@ static void _host_list_update(knet_handle_t knet_h)
 	}
 }
 
-int knet_host_add(knet_handle_t knet_h, uint8_t host_id)
+int knet_host_add(knet_handle_t knet_h, knet_node_id_t host_id)
 {
 	int savederrno = 0, err = 0;
 	struct knet_host *host = NULL;
@@ -109,7 +109,7 @@ exit_unlock:
 	return err;
 }
 
-int knet_host_remove(knet_handle_t knet_h, uint8_t host_id)
+int knet_host_remove(knet_handle_t knet_h, knet_node_id_t host_id)
 {
 	int savederrno = 0, err = 0;
 	struct knet_host *host, *removed;
@@ -181,7 +181,7 @@ exit_unlock:
 	return err;
 }
 
-int knet_host_set_name(knet_handle_t knet_h, uint8_t host_id, const char *name)
+int knet_host_set_name(knet_handle_t knet_h, knet_node_id_t host_id, const char *name)
 {
 	int savederrno = 0, err = 0;
 	struct knet_host *host;
@@ -241,7 +241,7 @@ exit_unlock:
 	return err;
 }
 
-int knet_host_get_name_by_host_id(knet_handle_t knet_h, uint8_t host_id,
+int knet_host_get_name_by_host_id(knet_handle_t knet_h, knet_node_id_t host_id,
 				  char *name)
 {
 	int savederrno = 0, err = 0;
@@ -280,7 +280,7 @@ exit_unlock:
 }
 
 int knet_host_get_id_by_host_name(knet_handle_t knet_h, const char *name,
-				  uint8_t *host_id)
+				  knet_node_id_t *host_id)
 {
 	int savederrno = 0, err = 0, found = 0;
 	struct knet_host *host;
@@ -327,7 +327,7 @@ int knet_host_get_id_by_host_name(knet_handle_t knet_h, const char *name,
 }
 
 int knet_host_get_host_list(knet_handle_t knet_h,
-			    uint8_t *host_ids, size_t *host_ids_entries)
+			    knet_node_id_t *host_ids, size_t *host_ids_entries)
 {
 	int savederrno = 0, err = 0;
 
@@ -357,7 +357,7 @@ int knet_host_get_host_list(knet_handle_t knet_h,
 	return err;
 }
 
-int knet_host_set_policy(knet_handle_t knet_h, uint8_t host_id,
+int knet_host_set_policy(knet_handle_t knet_h, knet_node_id_t host_id,
 			 uint8_t policy)
 {
 	int savederrno = 0, err = 0;
@@ -408,7 +408,7 @@ exit_unlock:
 	return err;
 }
 
-int knet_host_get_policy(knet_handle_t knet_h, uint8_t host_id,
+int knet_host_get_policy(knet_handle_t knet_h, knet_node_id_t host_id,
 			 uint8_t *policy)
 {
 	int savederrno = 0, err = 0;
@@ -447,7 +447,7 @@ exit_unlock:
 	return err;
 }
 
-int knet_host_get_status(knet_handle_t knet_h, uint8_t host_id,
+int knet_host_get_status(knet_handle_t knet_h, knet_node_id_t host_id,
 			 struct knet_host_status *status)
 {
 	int savederrno = 0, err = 0;
@@ -492,7 +492,7 @@ int knet_host_enable_status_change_notify(knet_handle_t knet_h,
 					  void *host_status_change_notify_fn_private_data,
 					  void (*host_status_change_notify_fn) (
 						void *private_data,
-						uint8_t host_id,
+						knet_node_id_t host_id,
 						uint8_t reachable,
 						uint8_t remote,
 						uint8_t external))
@@ -622,7 +622,7 @@ void _seq_num_set(struct knet_host *host, seq_num_t seq_num, int defrag_buf)
 int _host_dstcache_update_async(knet_handle_t knet_h, struct knet_host *host)
 {
 	int savederrno = 0;
-	uint8_t host_id = host->host_id;
+	knet_node_id_t host_id = host->host_id;
 
 	if (sendto(knet_h->dstsockfd[1], &host_id, sizeof(host_id), MSG_DONTWAIT | MSG_NOSIGNAL, NULL, 0) != sizeof(host_id)) {
 		savederrno = errno;

commit 5de918c8ff98f9a3cc0b42e910b0ea939a6b93b9

libknet.h: move the comment to appropriate place
Fixes: aff0a346

Signed-off-by: Chen Jingpiao <jchen@suse.com>

libknet/libknet.h
@@ -24,6 +24,8 @@
  * Kronosnet is an advanced VPN system for High Availability applications.
  */
 
+typedef uint16_t knet_node_id_t;
+
 /*
  * libknet limits
  */
@@ -31,8 +33,6 @@
 /*
  * Maximum number of hosts
  */
-
-typedef uint16_t knet_node_id_t;
 
 #define KNET_MAX_HOST 65536
 