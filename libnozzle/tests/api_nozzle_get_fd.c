e4512ac7bb3ff9b3a96b43f28168f546ee9ae877
Fabio M. Di Nitto <fdinitto@redhat.com>
2018-12-10 05:39:36 +0100

    [libnozzle] add nozzle_get_fd test
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

---
+++ libnozzle/tests/api_nozzle_get_fd.c
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2010-2018 Red Hat, Inc.  All rights reserved.
+ *
+ * Author: Fabio M. Di Nitto <fabbione@kronosnet.org>
+ *
+ * This software licensed under GPL-2.0+, LGPL-2.0+
+ */
+
+#include "config.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdint.h>
+#include <limits.h>
+#include <fcntl.h>
+
+#include "test-common.h"
+
+static int test(void)
+{
+	char device_name[IFNAMSIZ];
+	size_t size = IFNAMSIZ;
+	int err=0;
+	nozzle_t nozzle;
+	int fd;
+
+	printf("Testing get fd\n");
+
+	memset(device_name, 0, size);
+	nozzle = nozzle_open(device_name, size, NULL);
+	if (!nozzle) {
+		printf("Unable to init %s\n", device_name);
+		return -1;
+	}
+
+	fd = nozzle_get_fd(nozzle);
+	if (fd < 0) {
+		printf("Unable to get fd\n");
+		err = -1;
+		goto out_clean;
+	}
+
+	if (fcntl(fd, F_GETFD) < 0) {
+		printf("Unable to get valid fd\n");
+		err = -1;
+		goto out_clean;
+	}
+
+	printf("Testing ERROR conditions\n");
+
+	printf("Passing empty struct to get_fd\n");
+	if (nozzle_get_fd(NULL) > 0) {
+		printf("Something is wrong in nozzle_get_fd sanity checks\n");
+		err = -1;
+		goto out_clean;
+	}
+
+out_clean:
+	if (nozzle) {
+		nozzle_close(nozzle);
+	}
+
+	return err;
+}
+
+int main(void)
+{
+	need_root();
+
+	if (test() < 0)
+		return FAIL;
+
+	return PASS;
+}
