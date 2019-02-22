fb462d4f238b98ab685f9efae945a0e527f399ba
Fabio M. Di Nitto <fdinitto@redhat.com>
2019-02-22 06:53:48 +0100

    [access lists] make internal API consistent
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- libknet/links_acl.h
+++ libknet/links_acl.h
@@ -37,6 +37,6 @@ int check_rm(knet_handle_t knet_h, int sock, uint8_t transport,
 	     struct sockaddr_storage *ip1, struct sockaddr_storage *ip2,
 	     check_type_t type, check_acceptreject_t acceptreject);
 void check_rmall(knet_handle_t knet_h, int sock, uint8_t transport);
-int check_validate(knet_handle_t knet_h, int sockfd, struct sockaddr_storage *checkip);
+int check_validate(knet_handle_t knet_h, int sock, uint8_t transport, struct sockaddr_storage *checkip);
 
 #endif
