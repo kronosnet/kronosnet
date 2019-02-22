34d87fab04c1e1329f0066adf595d575dac3d0de
Fabio M. Di Nitto <fdinitto@redhat.com>
2019-02-22 05:21:29 +0100

    [access lists] cleanup API a bit
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- libknet/links_acl.h
+++ libknet/links_acl.h
@@ -37,8 +37,9 @@ int check_rm(knet_handle_t knet_h, int sock, uint8_t transport,
 	     struct sockaddr_storage *ip1, struct sockaddr_storage *ip2,
 	     check_type_t type, check_acceptreject_t acceptreject);
 void check_rmall(knet_handle_t knet_h, int sock, uint8_t transport);
+int check_validate(knet_handle_t knet_h, int sockfd, struct sockaddr_storage *checkip);
+
 int _link_add_default_acl(knet_handle_t knet_h, struct knet_link *kh_link);
 int _link_rm_default_acl(knet_handle_t knet_h, struct knet_link *kh_link);
-int _generic_filter_packet_by_acl(knet_handle_t knet_h, int sockfd, struct sockaddr_storage *checkip);
 
 #endif
