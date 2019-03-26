ec84794b9d48faed86bf9ac1325e85dc20704529
Fabio M. Di Nitto <fdinitto@redhat.com>
2019-03-26 13:45:52 +0100

    [global] update copyright across the board
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- libknet/threads_heartbeat.c
+++ libknet/threads_heartbeat.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2018 Red Hat, Inc.  All rights reserved.
+ * Copyright (C) 2015-2019 Red Hat, Inc.  All rights reserved.
  *
  * Authors: Fabio M. Di Nitto <fabbione@kronosnet.org>
  *          Federico Simoncelli <fsimon@kronosnet.org>
