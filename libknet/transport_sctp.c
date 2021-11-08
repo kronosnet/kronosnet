kronosnet/kronosnet

commit b16e6867323e1ff636fe30d9048007f0346017de

transport: Tidy internal transport API
Fix some comments, and make calls that return
values other than -1 & 0 use enums for clarity.

Signed-off-by: Christine Caulfield <ccaulfie@redhat.com>

libknet/transport_sctp.c
@@ -331,14 +331,14 @@ int sctp_transport_tx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err,
 		switch (knet_h->knet_transport_fd_tracker[sockfd].data_type) {
 			case SCTP_CONNECT_LINK_INFO:
 				if (connect_info->link->transport_connected == 0) {
-					return -1;
+					return KNET_TRANSPORT_SOCK_ERROR_INTERNAL;
 				}
 				break;
 			case SCTP_ACCEPTED_LINK_INFO:
 				listen_info = accepted_info->link_info;
 				if (listen_info->listen_sock != sockfd) {
 					if (listen_info->on_rx_epoll == 0) {
-						return -1;
+						return -KNET_TRANSPORT_SOCK_ERROR_INTERNAL;
 					}
 				}
 				break;
@@ -348,11 +348,11 @@ int sctp_transport_tx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err,
 			log_debug(knet_h, KNET_SUB_TRANSP_SCTP, "Sock: %d is overloaded. Slowing TX down", sockfd);
 #endif
 			_lock_sleep_relock(knet_h);
-			return 1;
+			return KNET_TRANSPORT_SOCK_ERROR_RETRY;
 		}
-		return -1;
+		return KNET_TRANSPORT_SOCK_ERROR_INTERNAL;
 	}
-	return 0;
+	return KNET_TRANSPORT_SOCK_ERROR_IGNORE;
 }
 
 /*
@@ -377,7 +377,7 @@ int sctp_transport_tx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err,
  * basically this small abuse of recv_err is to detect notifications
  * generated by sockets created by listen().
  */
-int sctp_transport_rx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err, int recv_errno)
+transport_sock_error_t sctp_transport_rx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err, int recv_errno)
 {
 	struct epoll_event ev;
 	sctp_accepted_link_info_t *accepted_info = knet_h->knet_transport_fd_tracker[sockfd].data;
@@ -446,7 +446,7 @@ int sctp_transport_rx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err,
  *       delegate any FD error management to sctp_transport_rx_sock_error
  *       and keep this code to parsing incoming data only
  */
-int sctp_transport_rx_is_data(knet_handle_t knet_h, int sockfd, struct knet_mmsghdr *msg)
+transport_rx_isdata_t sctp_transport_rx_is_data(knet_handle_t knet_h, int sockfd, struct knet_mmsghdr *msg)
 {
 	size_t i;
 	struct iovec *iov = msg->msg_hdr.msg_iov;

libknet/internals.h
@@ -367,6 +367,33 @@ typedef enum {
 #define TRANSPORT_PROTO_NOT_CONNECTION_ORIENTED 0
 #define TRANSPORT_PROTO_IS_CONNECTION_ORIENTED  1
 
+/*
+ * Returns from transport_tx_sock_error
+ */
+typedef enum {
+	KNET_TRANSPORT_RX_ERROR = -1,
+	KNET_TRANSPORT_SOCK_ERROR_INTERNAL = -1,
+	KNET_TRANSPORT_SOCK_ERROR_IGNORE = 0,
+	KNET_TRANSPORT_SOCK_ERROR_RETRY = 1,
+} transport_sock_error_t;
+
+/*
+ * Returns from transport_rx_is_data
+ */
+typedef enum {
+	KNET_TRANSPORT_RX_ISDATA_ERROR = -1,
+	KNET_TRANSPORT_RX_NOT_DATA_CONTINUE = 0,
+	KNET_TRANSPORT_RX_NOT_DATA_STOP = 1,
+	KNET_TRANSPORT_RX_IS_DATA = 2,
+	/* These two are only really used by SCTP */
+	KNET_TRANSPORT_RX_OOB_DATA_CONTINUE = 3,
+	KNET_TRANSPORT_RX_OOB_DATA_STOP = 4
+} transport_rx_isdata_t;
+
+
+/*
+ * All functions that return int return 0 for success, and -1 for failure.
+ */
 typedef struct knet_transport_ops {
 /*
  * transport generic information
@@ -401,7 +428,7 @@ typedef struct knet_transport_ops {
 /*
  * link operations should take care of all the
  * sockets and epoll management for a given link/transport set
- * transport_link_disable should return err = -1 and errno = EBUSY
+ * transport_link_disable should return -1 and errno = EBUSY
  * if listener is still in use, and any other errno in case
  * the link cannot be disabled.
  *
@@ -434,28 +461,21 @@ typedef struct knet_transport_ops {
  * transport_tx_sock_error is invoked with global_rwlock and
  * it's invoked when sendto or sendmmsg returns =< 0
  *
- * it should return:
- * -1 on internal error
- *  0 ignore error and continue
- *  1 retry
+ * it should return a transport_sock_error_t
  *    any sleep or wait action should happen inside the transport code
  */
-	int (*transport_tx_sock_error)(knet_handle_t knet_h, int sockfd, int recv_err, int recv_errno);
+	transport_sock_error_t (*transport_tx_sock_error)(knet_handle_t knet_h, int sockfd, int recv_err, int recv_errno);
 
 /*
  * this function is called on _every_ received packet
  * to verify if the packet is data or internal protocol error handling
  *
- * it should return:
- * -1 on error
- *  0 packet is not data and we should continue the packet process loop
- *  1 packet is not data and we should STOP the packet process loop
- *  2 packet is data and should be parsed as such
+ * it should return a transport_tx_isdata_t
  *
  * transport_rx_is_data is invoked with both global_rwlock
  * and fd_tracker read lock (from RX thread)
  */
-	int (*transport_rx_is_data)(knet_handle_t knet_h, int sockfd, struct knet_mmsghdr *msg);
+	transport_rx_isdata_t (*transport_rx_is_data)(knet_handle_t knet_h, int sockfd, struct knet_mmsghdr *msg);
 
 /*
  * this function is called by links.c when a link down event is recorded

libknet/threads_heartbeat.c
@@ -131,7 +131,7 @@ retry:
 		if (len != outlen) {
 			err = transport_tx_sock_error(knet_h, dst_link->transport, dst_link->outsock, len, savederrno);
 			switch(err) {
-				case -1: /* unrecoverable error */
+				case KNET_TRANSPORT_SOCK_ERROR_INTERNAL:
 					log_debug(knet_h, KNET_SUB_HEARTBEAT,
 						  "Unable to send ping (sock: %d) packet (sendto): %d %s. recorded src ip: %s src port: %s dst ip: %s dst port: %s",
 						  dst_link->outsock, savederrno, strerror(savederrno),
@@ -139,9 +139,9 @@ retry:
 						  dst_link->status.dst_ipaddr, dst_link->status.dst_port);
 					dst_link->status.stats.tx_ping_errors++;
 					break;
-				case 0:
+				case KNET_TRANSPORT_SOCK_ERROR_IGNORE:
 					break;
-				case 1:
+				case KNET_TRANSPORT_SOCK_ERROR_RETRY:
 					dst_link->status.stats.tx_ping_retries++;
 					goto retry;
 					break;
@@ -153,7 +153,7 @@ retry:
 	}
 
 	timespec_diff(pong_last, clock_now, &diff_ping);
-	if ((pong_last.tv_nsec) && 
+	if ((pong_last.tv_nsec) &&
 	    (diff_ping >= (dst_link->pong_timeout_adj * 1000llu))) {
 		_link_down(knet_h, dst_host, dst_link);
 	}
@@ -211,7 +211,7 @@ retry:
 		if (len != outlen) {
 			err = transport_tx_sock_error(knet_h, src_link->transport, src_link->outsock, len, savederrno);
 			switch(err) {
-				case -1: /* unrecoverable error */
+				case KNET_TRANSPORT_SOCK_ERROR_INTERNAL:
 					log_debug(knet_h, KNET_SUB_HEARTBEAT,
 						  "Unable to send pong reply (sock: %d) packet (sendto): %d %s. recorded src ip: %s src port: %s dst ip: %s dst port: %s",
 						  src_link->outsock, errno, strerror(errno),
@@ -219,9 +219,9 @@ retry:
 						  src_link->status.dst_ipaddr, src_link->status.dst_port);
 					src_link->status.stats.tx_pong_errors++;
 					break;
-				case 0: /* ignore error and continue */
+				case KNET_TRANSPORT_SOCK_ERROR_IGNORE:
 					break;
-				case 1: /* retry to send those same data */
+				case KNET_TRANSPORT_SOCK_ERROR_RETRY:
 					src_link->status.stats.tx_pong_retries++;
 					goto retry;
 					break;

libknet/threads_pmtud.c
@@ -249,7 +249,7 @@ retry:
 
 	err = transport_tx_sock_error(knet_h, dst_link->transport, dst_link->outsock, len, savederrno);
 	switch(err) {
-		case -1: /* unrecoverable error */
+		case KNET_TRANSPORT_SOCK_ERROR_INTERNAL:
 			log_debug(knet_h, KNET_SUB_PMTUD, "Unable to send pmtu packet (sendto): %d %s", savederrno, strerror(savederrno));
 			pthread_mutex_unlock(&knet_h->tx_mutex);
 			pthread_mutex_unlock(&knet_h->pmtud_mutex);
@@ -257,9 +257,9 @@ retry:
 			pthread_mutex_unlock(&dst_link->link_stats_mutex);
 			return -1;
 			break;
-		case 0: /* ignore error and continue */
+		case KNET_TRANSPORT_SOCK_ERROR_IGNORE:
 			break;
-		case 1: /* retry to send those same data */
+		case KNET_TRANSPORT_SOCK_ERROR_RETRY:
 			dst_link->status.stats.tx_pmtu_retries++;
 			goto retry;
 			break;
@@ -720,7 +720,7 @@ retry:
 				return;
 			}
 			switch(err) {
-				case -1: /* unrecoverable error */
+				case KNET_TRANSPORT_SOCK_ERROR_INTERNAL:
 					log_debug(knet_h, KNET_SUB_PMTUD,
 						  "Unable to send PMTUd reply (sock: %d) packet (sendto): %d %s. recorded src ip: %s src port: %s dst ip: %s dst port: %s",
 						  src_link->outsock, errno, strerror(errno),
@@ -729,10 +729,10 @@ retry:
 
 					src_link->status.stats.tx_pmtu_errors++;
 					break;
-				case 0: /* ignore error and continue */
+				case KNET_TRANSPORT_SOCK_ERROR_IGNORE:
 					src_link->status.stats.tx_pmtu_errors++;
 					break;
-				case 1: /* retry to send those same data */
+				case KNET_TRANSPORT_SOCK_ERROR_RETRY:
 					src_link->status.stats.tx_pmtu_retries++;
 					pthread_mutex_unlock(&src_link->link_stats_mutex);
 					goto retry;

libknet/threads_tx.c
@@ -83,13 +83,13 @@ retry:
 
 		err = transport_tx_sock_error(knet_h, dst_host->link[dst_host->active_links[link_idx]].transport, dst_host->link[dst_host->active_links[link_idx]].outsock, sent_msgs, savederrno);
 		switch(err) {
-			case -1: /* unrecoverable error */
+			case KNET_TRANSPORT_SOCK_ERROR_INTERNAL:
 				cur_link->status.stats.tx_data_errors++;
 				goto out_unlock;
 				break;
-			case 0: /* ignore error and continue */
+			case KNET_TRANSPORT_SOCK_ERROR_IGNORE:
 				break;
-			case 1: /* retry to send those same data */
+			case KNET_TRANSPORT_SOCK_ERROR_RETRY:
 				cur_link->status.stats.tx_data_retries++;
 				goto retry;
 				break;

libknet/transport_loopback.c
@@ -59,14 +59,14 @@ int loopback_transport_rx_sock_error(knet_handle_t knet_h, int sockfd, int recv_
 	return 0;
 }
 
-int loopback_transport_tx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err, int recv_errno)
+transport_sock_error_t loopback_transport_tx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err, int recv_errno)
 {
-	return 0;
+	return KNET_TRANSPORT_SOCK_ERROR_IGNORE;
 }
 
-int loopback_transport_rx_is_data(knet_handle_t knet_h, int sockfd, struct knet_mmsghdr *msg)
+transport_rx_isdata_t loopback_transport_rx_is_data(knet_handle_t knet_h, int sockfd, struct knet_mmsghdr *msg)
 {
-	return 0;
+	return KNET_TRANSPORT_RX_NOT_DATA_CONTINUE;
 }
 
 int loopback_transport_link_dyn_connect(knet_handle_t knet_h, int sockfd, struct knet_link *kn_link)

libknet/transport_loopback.h
@@ -20,8 +20,8 @@ int loopback_transport_link_clear_config(knet_handle_t knet_h, struct knet_link 
 int loopback_transport_free(knet_handle_t knet_h);
 int loopback_transport_init(knet_handle_t knet_h);
 int loopback_transport_rx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err, int recv_errno);
-int loopback_transport_tx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err, int recv_errno);
-int loopback_transport_rx_is_data(knet_handle_t knet_h, int sockfd, struct knet_mmsghdr *msg);
+transport_sock_error_t loopback_transport_tx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err, int recv_errno);
+transport_rx_isdata_t loopback_transport_rx_is_data(knet_handle_t knet_h, int sockfd, struct knet_mmsghdr *msg);
 int loopback_transport_link_dyn_connect(knet_handle_t knet_h, int sockfd, struct knet_link *kn_link);
 int loopback_transport_link_is_down(knet_handle_t knet_h, struct knet_link *kn_link);
 

libknet/transport_sctp.h
@@ -28,8 +28,8 @@ int sctp_transport_link_clear_config(knet_handle_t knet_h, struct knet_link *kn_
 int sctp_transport_free(knet_handle_t knet_h);
 int sctp_transport_init(knet_handle_t knet_h);
 int sctp_transport_rx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err, int recv_errno);
-int sctp_transport_tx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err, int recv_errno);
-int sctp_transport_rx_is_data(knet_handle_t knet_h, int sockfd, struct knet_mmsghdr *msg);
+transport_sock_error_t sctp_transport_tx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err, int recv_errno);
+transport_rx_isdata_t sctp_transport_rx_is_data(knet_handle_t knet_h, int sockfd, struct knet_mmsghdr *msg);
 int sctp_transport_link_dyn_connect(knet_handle_t knet_h, int sockfd, struct knet_link *kn_link);
 int sctp_transport_link_is_down(knet_handle_t knet_h, struct knet_link *kn_link);
 

commit 8095da6ed63358753b2a07b760ad5dc9b75a9fe8

[sctp] fix return code
regression from b16e6867323e1ff636fe30d9048007f0346017de

Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

libknet/transport_sctp.c
@@ -338,7 +338,7 @@ int sctp_transport_tx_sock_error(knet_handle_t knet_h, int sockfd, int recv_err,
 				listen_info = accepted_info->link_info;
 				if (listen_info->listen_sock != sockfd) {
 					if (listen_info->on_rx_epoll == 0) {
-						return -KNET_TRANSPORT_SOCK_ERROR_INTERNAL;
+						return KNET_TRANSPORT_SOCK_ERROR_INTERNAL;
 					}
 				}
 				break;