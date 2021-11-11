kronosnet/kronosnet

commit 7e89a6c4917479786e6a62977b991789f659cd19

[debug] add onwire-v1-extra-debug build option
this option is strictly for debugging of onwire v1 protocol.

IT BREAKS ONWIRE COMPATIBILITY! DO NOT ENABLE JUST TO ADD FEATURES.

It will eventually fold into v2 protocol by default.

Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

libknet/threads_rx.c
@@ -692,6 +692,17 @@ static void _process_data(knet_handle_t knet_h, struct knet_host *src_host, stru
 		return;
 	}
 
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+	if (inbuf->khp_data_v1_checksum != compute_chksum(data, len - header_size)) {
+		log_err(knet_h, KNET_SUB_RX, "Received incorrect data checksum after reassembly from host: %u seq: %u", src_host->host_id, seq_num);
+		/*
+		 * give a chance to the log threads to pick up the message
+		 */
+		sleep(1);
+		abort();
+	}
+#endif
+
 	if (_deliver_data(knet_h, data, len, header_size, channel) < 0) {
 		return;
 	}
@@ -744,10 +755,28 @@ static struct knet_header *_decrypt_packet(knet_handle_t knet_h, struct knet_hea
 
 static int _packet_checks(knet_handle_t knet_h, struct knet_header *inbuf, ssize_t len)
 {
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+	uint32_t rx_packet_checksum, expected_packet_checksum;
+#endif
+
 	if (len < (ssize_t)(KNET_HEADER_SIZE + 1)) {
 		log_debug(knet_h, KNET_SUB_RX, "Packet is too short: %ld", (long)len);
 		return -1;
 	}
+
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+	rx_packet_checksum = inbuf->kh_checksum;
+	inbuf->kh_checksum = 0;
+	expected_packet_checksum = compute_chksum((const unsigned char *)inbuf, len);
+	if (rx_packet_checksum != expected_packet_checksum) {
+		log_err(knet_h, KNET_SUB_RX, "Received packet with incorrect checksum. Received: %u Expected: %u", rx_packet_checksum, expected_packet_checksum);
+		/*
+		 * give a chance to the log threads to pick up the message
+		 */
+		sleep(1);
+		abort();
+	}
+#endif
 
 	/*
 	 * old versions of knet did not advertise max_ver and max_ver is set to 0.
@@ -861,11 +890,11 @@ static void _parse_recv_from_links(knet_handle_t knet_h, int sockfd, const struc
 		return;
 	}
 
-	inbuf->kh_node = ntohs(inbuf->kh_node);
-
 	if (_packet_checks(knet_h, inbuf, len) < 0) {
 		return;
 	}
+
+	inbuf->kh_node = ntohs(inbuf->kh_node);
 
 	/*
 	 * determine source host

configure.ac
@@ -364,6 +364,13 @@ AC_SUBST([libnozzlerustver])
 AC_ARG_ENABLE([debug],
 	[AS_HELP_STRING([--enable-debug],[enable debug build])])
 
+AC_ARG_ENABLE([onwire-v1-extra-debug],
+	[AS_HELP_STRING([--enable-onwire-v1-extra-debug],[enable onwire protocol v1 extra debug. WARNING: IT BREAKS ONWIRE COMPATIBILITY! DO NOT USE IN PRODUCTION!])])
+
+if test "x${enable_onwire_v1_extra_debug}" = xyes; then
+	AC_DEFINE_UNQUOTED([ONWIRE_V1_EXTRA_DEBUG], [1], [Enable crc32 checksum for data and packets])
+fi
+
 # for standard crc32 function (used in test suite)
 PKG_CHECK_MODULES([zlib], [zlib])
 

libknet/onwire.h
@@ -65,6 +65,9 @@ struct knet_header_payload_data_v1 {
 	uint8_t		khp_data_frag_num;	/* number of fragments of this pckt. 1 is not fragmented */
 	uint8_t		khp_data_frag_seq;	/* as above, indicates the frag sequence number */
 	int8_t		khp_data_channel;	/* transport channel data for localsock <-> knet <-> localsock mapping */
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+	uint32_t	khp_data_checksum;	/* debug checksum of all user data */
+#endif
 	uint8_t		khp_data_userdata[0];	/* pointer to the real user data */
 } __attribute__((packed));
 
@@ -75,6 +78,9 @@ struct knet_header_payload_data_v1 {
 #define khp_data_v1_bcast    kh_payload.khp_data_v1.khp_data_bcast
 #define khp_data_v1_channel  kh_payload.khp_data_v1.khp_data_channel
 #define khp_data_v1_compress kh_payload.khp_data_v1.khp_data_compress
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+#define khp_data_v1_checksum kh_payload.khp_data_v1.khp_data_checksum
+#endif
 
 /*
  * KNET_HEADER_TYPE_PING / KNET_HEADER_TYPE_PONG
@@ -150,6 +156,9 @@ struct knet_header {
 	knet_node_id_t			kh_node;    /* host id of the source host for this pckt */
 	uint8_t				kh_max_ver; /* max version of the protocol supported by this node */
 	uint8_t				kh_pad1;    /* make sure to have space in the header to grow features */
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+	uint32_t			kh_checksum;/* debug checksum per packet */
+#endif
 	union knet_header_payload	kh_payload; /* union of potential data struct based on kh_type */
 } __attribute__((packed));
 

libknet/onwire_v1.c
@@ -40,6 +40,11 @@ int prep_ping_v1(knet_handle_t knet_h, struct knet_link *dst_link, uint8_t onwir
 	knet_h->pingbuf->khp_ping_v1_seq_num = htons(knet_h->tx_seq_num);
 	pthread_mutex_unlock(&knet_h->tx_seq_num_mutex);
 
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+	knet_h->pingbuf->kh_checksum = 0;
+	knet_h->pingbuf->kh_checksum = compute_chksum((const unsigned char*)knet_h->pingbuf, KNET_HEADER_PING_V1_SIZE);
+#endif
+
 	return 0;
 }
 
@@ -48,6 +53,11 @@ void prep_pong_v1(knet_handle_t knet_h, struct knet_header *inbuf, ssize_t *outl
 	*outlen = KNET_HEADER_PING_V1_SIZE;
 	inbuf->kh_type = KNET_HEADER_TYPE_PONG;
 	inbuf->kh_node = htons(knet_h->host_id);
+
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+	inbuf->kh_checksum = 0;
+	inbuf->kh_checksum = compute_chksum((const unsigned char*)inbuf, KNET_HEADER_PING_V1_SIZE);
+#endif
 }
 
 void process_ping_v1(knet_handle_t knet_h, struct knet_host *src_host, struct knet_link *src_link, struct knet_header *inbuf, ssize_t len)
@@ -103,7 +113,7 @@ struct knet_link *get_link_from_pong_v1(knet_handle_t knet_h, struct knet_host *
 	return &src_host->link[inbuf->khp_ping_v1_link];
 }
 
-void prep_pmtud_v1(knet_handle_t knet_h, struct knet_link *dst_link, uint8_t onwire_ver, size_t onwire_len)
+void prep_pmtud_v1(knet_handle_t knet_h, struct knet_link *dst_link, uint8_t onwire_ver, size_t onwire_len, size_t data_len)
 {
 	knet_h->pmtudbuf->kh_version = onwire_ver;
 	knet_h->pmtudbuf->kh_max_ver = knet_h->onwire_max_ver;
@@ -111,6 +121,11 @@ void prep_pmtud_v1(knet_handle_t knet_h, struct knet_link *dst_link, uint8_t onw
 	knet_h->pmtudbuf->kh_node = htons(knet_h->host_id);
 	knet_h->pmtudbuf->khp_pmtud_v1_link = dst_link->link_id;
 	knet_h->pmtudbuf->khp_pmtud_v1_size = onwire_len;
+
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+	knet_h->pmtudbuf->kh_checksum = 0;
+	knet_h->pmtudbuf->kh_checksum = compute_chksum((const unsigned char*)knet_h->pmtudbuf, data_len);
+#endif
 }
 
 void prep_pmtud_reply_v1(knet_handle_t knet_h, struct knet_header *inbuf, ssize_t *outlen)
@@ -118,6 +133,11 @@ void prep_pmtud_reply_v1(knet_handle_t knet_h, struct knet_header *inbuf, ssize_
 	*outlen = KNET_HEADER_PMTUD_V1_SIZE;
 	inbuf->kh_type = KNET_HEADER_TYPE_PMTUD_REPLY;
 	inbuf->kh_node = htons(knet_h->host_id);
+
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+	inbuf->kh_checksum = 0;
+	inbuf->kh_checksum = compute_chksum((const unsigned char*)inbuf, KNET_HEADER_PMTUD_V1_SIZE);
+#endif
 }
 
 void process_pmtud_reply_v1(knet_handle_t knet_h, struct knet_link *src_link, struct knet_header *inbuf)
@@ -126,7 +146,7 @@ void process_pmtud_reply_v1(knet_handle_t knet_h, struct knet_link *src_link, st
 }
 
 void prep_tx_bufs_v1(knet_handle_t knet_h,
-		     struct knet_header *inbuf, unsigned char *data, size_t inlen, unsigned int temp_data_mtu,
+		     struct knet_header *inbuf, unsigned char *data, size_t inlen, uint32_t data_checksum, unsigned int temp_data_mtu,
 		     seq_num_t tx_seq_num, int8_t channel, int bcast, int data_compressed,
 		     int *msgs_to_send, struct iovec iov_out[PCKT_FRAG_MAX][2], int *iovcnt_out)
 {
@@ -154,7 +174,9 @@ void prep_tx_bufs_v1(knet_handle_t knet_h,
 	} else {
 		inbuf->khp_data_v1_compress = 0;
 	}
-
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+	inbuf->khp_data_v1_checksum = data_checksum;
+#endif
 	/*
 	 * handle fragmentation
 	 */
@@ -185,6 +207,11 @@ void prep_tx_bufs_v1(knet_handle_t knet_h,
 			 */
 			knet_h->send_to_links_buf[frag_idx]->khp_data_v1_frag_seq = frag_idx + 1;
 
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+			knet_h->send_to_links_buf[frag_idx]->kh_checksum = 0;
+			knet_h->send_to_links_buf[frag_idx]->kh_checksum = compute_chksumv(iov_out[frag_idx], 2);
+#endif
+
 			frag_len = frag_len - temp_data_mtu;
 			frag_idx++;
 		}
@@ -193,6 +220,11 @@ void prep_tx_bufs_v1(knet_handle_t knet_h,
 		iov_out[frag_idx][0].iov_base = (void *)inbuf;
 		iov_out[frag_idx][0].iov_len = frag_len + KNET_HEADER_DATA_V1_SIZE;
 		*iovcnt_out = 1;
+
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+		inbuf->kh_checksum = 0;
+		inbuf->kh_checksum = compute_chksumv(iov_out[frag_idx], 1);
+#endif
 	}
 	*msgs_to_send = inbuf->khp_data_v1_frag_num;
 }

libknet/onwire_v1.h
@@ -19,12 +19,12 @@ void process_ping_v1(knet_handle_t knet_h, struct knet_host *src_host, struct kn
 void process_pong_v1(knet_handle_t knet_h, struct knet_host *src_host, struct knet_link *src_link, struct knet_header *inbuf, struct timespec *recvtime);
 struct knet_link *get_link_from_pong_v1(knet_handle_t knet_h, struct knet_host *src_host, struct knet_header *inbuf);
 
-void prep_pmtud_v1(knet_handle_t knet_h, struct knet_link *dst_link, uint8_t onwire_ver, size_t onwire_len);
+void prep_pmtud_v1(knet_handle_t knet_h, struct knet_link *dst_link, uint8_t onwire_ver, size_t onwire_len, size_t data_len);
 void prep_pmtud_reply_v1(knet_handle_t knet_h, struct knet_header *inbuf, ssize_t *outlen);
 void process_pmtud_reply_v1(knet_handle_t knet_h, struct knet_link *src_link, struct knet_header *inbuf);
 
 void prep_tx_bufs_v1(knet_handle_t knet_h,
-		     struct knet_header *inbuf, unsigned char *data, size_t inlen, unsigned int temp_data_mtu,
+		     struct knet_header *inbuf, unsigned char *data, size_t inlen, uint32_t data_checksum, unsigned int temp_data_mtu,
 		     seq_num_t tx_seq_num, int8_t channel, int bcast, int data_compressed,
 		     int *msgs_to_send, struct iovec iov_out[PCKT_FRAG_MAX][2], int *iovcnt_out);
 

libknet/threads_pmtud.c
@@ -146,11 +146,11 @@ restart:
 	data_len = app_mtu_len + knet_h->sec_hash_size + knet_h->sec_salt_size + KNET_HEADER_ALL_SIZE;
 
 	if (knet_h->onwire_ver_remap) {
-		prep_pmtud_v1(knet_h, dst_link, onwire_ver, onwire_len);
+		prep_pmtud_v1(knet_h, dst_link, onwire_ver, onwire_len, app_mtu_len + KNET_HEADER_ALL_SIZE);
 	} else {
 		switch (onwire_ver) {
 			case 1:
-				prep_pmtud_v1(knet_h, dst_link, onwire_ver, onwire_len);
+				prep_pmtud_v1(knet_h, dst_link, onwire_ver, onwire_len, app_mtu_len + KNET_HEADER_ALL_SIZE);
 				break;
 			default:
 				log_warn(knet_h, KNET_SUB_PMTUD, "preparing PMTUD onwire version %u not supported", onwire_ver);

libknet/threads_tx.c
@@ -175,7 +175,7 @@ out:
 
 static int _prep_tx_bufs(knet_handle_t knet_h,
 			  struct knet_header *inbuf, uint8_t onwire_ver,
-			  unsigned char *data, size_t inlen,
+			  unsigned char *data, size_t inlen, uint32_t data_checksum,
 			  seq_num_t tx_seq_num, int8_t channel, int bcast, int data_compressed,
 			  int *msgs_to_send, struct iovec iov_out[PCKT_FRAG_MAX][2], int *iovcnt_out)
 {
@@ -201,11 +201,11 @@ static int _prep_tx_bufs(knet_handle_t knet_h,
 	}
 
 	if (knet_h->onwire_ver_remap) {
-		prep_tx_bufs_v1(knet_h, inbuf, data, inlen, temp_data_mtu, tx_seq_num, channel, bcast, data_compressed, msgs_to_send, iov_out, iovcnt_out);
+		prep_tx_bufs_v1(knet_h, inbuf, data, inlen, data_checksum, temp_data_mtu, tx_seq_num, channel, bcast, data_compressed, msgs_to_send, iov_out, iovcnt_out);
 	} else {
 		switch (onwire_ver) {
 			case 1:
-				prep_tx_bufs_v1(knet_h, inbuf, data, inlen, temp_data_mtu, tx_seq_num, channel, bcast, data_compressed, msgs_to_send, iov_out, iovcnt_out);
+				prep_tx_bufs_v1(knet_h, inbuf, data, inlen, data_checksum, temp_data_mtu, tx_seq_num, channel, bcast, data_compressed, msgs_to_send, iov_out, iovcnt_out);
 				break;
 			default: /* this should never hit as filters are in place in the calling functions */
 				log_warn(knet_h, KNET_SUB_TX, "preparing data onwire version %u not supported", onwire_ver);
@@ -579,6 +579,7 @@ static int _parse_recv_from_sock(knet_handle_t knet_h, size_t inlen, int8_t chan
 	unsigned char *data;					/* onwire neutrual pointer to data to send */
 	int data_compressed = 0;				/* track data compression to fill the header */
 	seq_num_t tx_seq_num;
+	uint32_t data_checksum = 0;				/* used only for debugging at the moment */
 
 	int bcast = 1;						/* assume all packets are to be broadcasted unless filter tells us differently */
 	knet_node_id_t dst_host_ids[KNET_MAX_HOST];		/* store destinations from filter */
@@ -612,6 +613,10 @@ static int _parse_recv_from_sock(knet_handle_t knet_h, size_t inlen, int8_t chan
 		}
 	}
 
+#ifdef ONWIRE_V1_EXTRA_DEBUG
+	data_checksum = compute_chksum(data, inlen);
+#endif
+
 	memset(dst_host_ids, 0, sizeof(dst_host_ids));
 	err = _get_data_dests(knet_h, data, inlen,
 			      &channel, &bcast, &send_local,
@@ -643,7 +648,7 @@ static int _parse_recv_from_sock(knet_handle_t knet_h, size_t inlen, int8_t chan
 		goto out;
 	}
 
-	err = _prep_tx_bufs(knet_h, inbuf, onwire_ver, data, inlen, tx_seq_num, channel, bcast, data_compressed, &msgs_to_send, iov_out, &iovcnt_out);
+	err = _prep_tx_bufs(knet_h, inbuf, onwire_ver, data, inlen, data_checksum, tx_seq_num, channel, bcast, data_compressed, &msgs_to_send, iov_out, &iovcnt_out);
 	if (err < 0) {
 		savederrno = errno;
 		goto out;

commit a4f7b8122450f82cb94b4d45aa1255f874f0b206

[rx] fix logging for host ids
7e89a6c4917479786e6a62977b991789f659cd19 introduced a minor
regression when logging host ids in RX packet checks.

The change had no other side effects.

Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

libknet/threads_rx.c
@@ -765,6 +765,7 @@ static int _packet_checks(knet_handle_t knet_h, struct knet_header *inbuf, ssize
 	}
 
 #ifdef ONWIRE_V1_EXTRA_DEBUG
+	inbuf->kh_node = htons(inbuf->kh_node);
 	rx_packet_checksum = inbuf->kh_checksum;
 	inbuf->kh_checksum = 0;
 	expected_packet_checksum = compute_chksum((const unsigned char *)inbuf, len);
@@ -776,6 +777,7 @@ static int _packet_checks(knet_handle_t knet_h, struct knet_header *inbuf, ssize
 		sleep(1);
 		abort();
 	}
+	inbuf->kh_node = ntohs(inbuf->kh_node);
 #endif
 
 	/*
@@ -903,11 +905,11 @@ static void _parse_recv_from_links(knet_handle_t knet_h, int sockfd, const struc
 		return;
 	}
 
+	inbuf->kh_node = ntohs(inbuf->kh_node);
+
 	if (_packet_checks(knet_h, inbuf, len) < 0) {
 		return;
 	}
-
-	inbuf->kh_node = ntohs(inbuf->kh_node);
 
 	/*
 	 * determine source host